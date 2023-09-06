c1a612eee88b9dc2eb96bdd5996e0199c8c8280e
Christopher Brauns <chris@blocktrainapp.com>
2023-09-06 16:26:04 -0400

    Proof of concept job match

--- Gemfile
+++ Gemfile
@@ -56,7 +56,10 @@ end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
+  gem "pry"
   gem "web-console"
+  gem "spring"
+  gem 'spring-commands-rspec'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
---
+++ spec/job_match_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+# test the JobMatch class
+RSpec.describe JobMatch do
+  # subject { described_class.new(profile: profile, job: job) }
+
+  let(:job) { Match::Job.new(industry: "manufacturing") }
+
+  context "when the profile's industry interest includes the job's industry" do
+    let(:profile) { Match::Profile.new(industry_interests: ["manufacturing"]) }
+
+    it "returns a match score of 1" do
+      expect(described_class.new(profile: profile, job: job).match_score).to eq(1)
+    end
+  end
+
+  context "when the profile's industry interest does not include the job's industry" do
+    let(:profile) { Match::Profile.new(industry_interests: ["retail"]) }
+
+    it "returns a match score of 0" do
+      expect(described_class.new(profile: profile, job: job).match_score).to eq(0)
+    end
+  end
+end

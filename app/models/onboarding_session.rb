b2f243bf5ba3c9ba1de407b9b92fcddcb2521913
Christopher Brauns <chris@blocktrainapp.com>
2023-09-22 15:44:26 -0400

    Hardcode all interests for now

--- app/models/onboarding_session.rb
+++ app/models/onboarding_session.rb
@@ -2,10 +2,10 @@ class OnboardingSession < ApplicationRecord
   self.table_name = "OnboardingSession"
 
   def industry_interests
-    begin
-      responses["opportunityInterests"]["response"].map(&:downcase)
-    rescue
-      ['construction', 'manufacturing', 'healthcare']
-    end
+    # begin
+    #   responses["opportunityInterests"]["response"].map(&:downcase)
+    # rescue
+    ['construction', 'manufacturing', 'healthcare']
+    # end
   end
 end

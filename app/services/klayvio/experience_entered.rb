be3804af7ee15782d47e42a8c3e17637fecf8630
Christopher Brauns <chris@blocktrainapp.com>
2023-10-20 12:17:31 -0400

    Fix experience entered event

--- app/services/klayvio/experience_entered.rb
+++ app/services/klayvio/experience_entered.rb
@@ -1,8 +1,10 @@
 module Klayvio
   class ExperienceEntered
     def call(event:)
+      user = User.find(event.aggregate_id)
+
       Klayvio.new.experience_entered(
-        email: event.data["email"],
+        email: user.email,
         event_id: event.id,
         occurred_at: event.occurred_at
       )
--- spec/services/klayvio/experience_entered_spec.rb
+++ spec/services/klayvio/experience_entered_spec.rb
@@ -3,14 +3,19 @@ require 'rails_helper'
 RSpec.describe Klayvio::ExperienceEntered do
   describe "#call" do
     let(:event) do
-      build(:event, :experience_created)
+      build(
+        :event,
+        :experience_created,
+        aggregate_id: user.id,
+      )
     end
+    let(:user) { create(:user, email:) }
     let(:email) { "tom@blocktrainapp.com" }
     let(:occurred_at) { Date.new(2020, 1, 1) }
 
     it "calls the Klayvio API" do
       expect_any_instance_of(Klayvio::Klayvio).to receive(:experience_entered).with(
-        email: event.data["email"],
+        email:,
         event_id: event.id,
         occurred_at: event.occurred_at
       )

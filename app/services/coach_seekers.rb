17721b272388e1f5a43ae13c846a2d61a9812207
John Chabot <johnchabot2013@gmail.com>
2024-01-08 13:56:11 -0500

    Update add note to add an id

--- app/services/coach_seekers.rb
+++ app/services/coach_seekers.rb
@@ -28,12 +28,13 @@ class CoachSeekers
     serialize_coach_seeker_context(csc)
   end
 
-  def self.add_note(id, note, now: Time.now)
+  def self.add_note(id, note, note_id: SecureRandom.uuid, now: Time.now)
     CreateEventJob.perform_later(
       event_type: Event::EventTypes::NOTE_ADDED,
       aggregate_id: id,
       data: {
-        note:
+        note:,
+        note_id:
       },
       metadata: {},
       occurred_at: now

6025cb0b585d09c4a146e6e22cc1c368d48e9f99
John Chabot <johnchabot2013@gmail.com>
2024-01-10 16:51:37 -0500

    Else case for slack envvar not set

--- app/services/slack/gateway.rb
+++ app/services/slack/gateway.rb
@@ -1,11 +1,15 @@
 module Slack
   class Gateway
+    GatewayEnvvarNotSetError = Class.new(StandardError)
+
     def self.build
       case ENV["SLACK_GATEWAY_STRATEGY"]
       when "real"
         Slack::Notifier.new(ENV["SLACK_WEBHOOK_URL"])
       when "fake"
         FakeSlackGateway.new
+      else
+        raise GatewayEnvvarNotSetError
       end
     end
   end

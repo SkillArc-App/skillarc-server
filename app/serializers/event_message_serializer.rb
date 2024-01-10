c620d4935d992ad1bad9a4f4acb9843226509b22
John Chabot <johnchabot2013@gmail.com>
2024-01-09 19:37:49 -0500

    Add serializer for EventMessage

---
+++ app/serializers/event_message_serializer.rb
@@ -0,0 +1,33 @@
+class EventMessageSerializer < ActiveJob::Serializers::ObjectSerializer
+  def serialize?(argument)
+    argument.is_a?(klass)
+  end
+
+  def serialize(message)
+    super(
+      "id" => message.id,
+      "aggregate_id" => message.aggregate_id,
+      "event_type" => message.event_type,
+      "data" => message.data,
+      "metadata" => message.metadata,
+      "version" => message.version,
+      "occurred_at" => message.occurred_at,
+    )
+  end
+
+  def deserialize(hash)
+    klass.new(
+      id: hash["id"],
+      aggregate_id: hash["aggregate_id"],
+      event_type: hash["event_type"],
+      data: hash["data"],
+      metadata: hash["metadata"],
+      version: hash["version"],
+      occurred_at: Time.zone.parse(hash["occurred_at"])
+    )
+  end
+
+  def klass
+    EventMessage
+  end
+end

144afc0c64d47a1cc1b881a57eaae7875ce8b927
John Chabot <johnchabot2013@gmail.com>
2024-01-17 11:41:41 -0500

    Add spec for secured

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -23,19 +23,15 @@ module Secured
     auth_client = AuthClient::Factory.build
 
     validation_response = auth_client.validate_token(token)
-    _, sub = validation_response.decoded_token[0]['sub'].split('|')
-
-    user = user_finder.find_or_create(
-      sub:,
-      token:,
-      auth_client:
-    )
-
-    @current_user = user
-
-    return unless (error = validation_response.error)
-
-    render json: { message: error.message }, status: error.status
+    if validation_response.success?
+      @current_user = user_finder.find_or_create(
+        sub: validation_response.sub,
+        token:,
+        auth_client:
+      )
+    else
+      render json: { message: validation_response.message }, status: validation_response.status
+    end
   end
 
   private

48770312f0451b430241a7ca3494914531d56154
John Chabot <johnchabot2013@gmail.com>
2024-01-19 14:24:18 -0500

    Add missing profile factory

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :profile do
-    id { SecureRandom.uuid }
     user
+
+    id { SecureRandom.uuid }
+    bio { "I'm Mary and I'm a good worker" }
+    image { "www.images.com/my_image/123" }
+    met_career_coach { false }
+    status { nil } # column unused
   end
 end

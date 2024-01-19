ad484f5de3e1dff05f6e165fd93bc5a417a0eb16
John Chabot <johnchabot2013@gmail.com>
2024-01-19 14:02:41 -0500

    Add basic test controller spec

---
+++ spec/requests/test_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe "Test", type: :request do
+  describe "POST /create_test_user" do
+    subject { post create_user_test_path, params:, headers: }
+
+    let(:headers) { {} }
+    let(:params) { {} }
+
+    it "returns a 200" do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /create_coach" do
+    subject { post create_coach_test_path, params:, headers: }
+
+    let(:headers) { {} }
+    let(:params) { {} }
+
+    before do
+      create(:role, name: Role::Types::COACH)
+    end
+
+    it "returns a 200" do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /create_seeker" do
+    subject { post create_seeker_test_path, params:, headers: }
+
+    let(:headers) { {} }
+    let(:params) { {} }
+
+    it "returns a 200" do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /create_test_recruiter_with_applicant" do
+    subject { post create_recruiter_with_applicant_test_path, params:, headers: }
+
+    let(:headers) { {} }
+    let(:params) { {} }
+
+    it "returns a 200" do
+      subject
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /reset_database" do
+    # Not tested as it is sort of weird reseting the db in a test
+  end
+end

5dfdaf4a1e1baf5fdc67fde406e28f3edc6a6329
Christopher Brauns <chris@blocktrainapp.com>
2024-01-22 10:17:12 -0500

    Extract is_coach for reuse

--- app/controllers/concerns/coach_auth.rb
+++ app/controllers/concerns/coach_auth.rb
@@ -1,8 +1,14 @@
 module CoachAuth
   def coach_authorize
-    return if current_user.roles.any? { |r| r.name == Role::Types::COACH }
+    return if is_coach?
 
     render json: { error: 'Not authorized' }, status: :unauthorized
     nil
+  end
+
+  def is_coach?
+    return false unless current_user
+
+    current_user.roles.any? { |r| r.name == Role::Types::COACH }
   end
 end

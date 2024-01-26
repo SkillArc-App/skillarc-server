b357146fb9a1345ec889f70110b04bf92f91664a
John Chabot <johnchabot2013@gmail.com>
2024-01-26 11:55:57 -0500

    Update EventService to take an Events::Schema

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -1,13 +1,21 @@
 class EventService
-  def self.create!(event_type:, aggregate_id:, data:, id: SecureRandom.uuid, occurred_at: Time.zone.now, metadata: {}, version: 1) # rubocop:disable Metrics/ParameterLists
+  NotEventSchemaError = Class.new(StandardError)
+  InvalidSchemaError = Class.new(StandardError)
+
+  def self.create!(event_schema:, aggregate_id:, data:, id: SecureRandom.uuid, occurred_at: Time.zone.now, metadata: {}) # rubocop:disable Metrics/ParameterLists
+    raise NotEventSchemaError unless event_schema.is_a?(Events::Schema)
+
+    raise InvalidSchemaError unless event_schema.data === data # rubocop:disable Style/CaseEquality
+    raise InvalidSchemaError unless event_schema.metadata === metadata # rubocop:disable Style/CaseEquality
+
     message = Events::Message.new(
       id:,
-      event_type:,
       aggregate_id:,
       occurred_at:,
-      version:,
-      data: data.deep_symbolize_keys,
-      metadata: metadata.deep_symbolize_keys
+      data: data.to_h,
+      metadata: metadata.to_h,
+      event_type: event_schema.event_type,
+      version: event_schema.version
     )
 
     Event.from_message!(message)
--- spec/services/event_service_spec.rb
+++ spec/services/event_service_spec.rb
@@ -5,12 +5,11 @@ RSpec.describe EventService do
     subject do
       described_class.create!(
         id:,
-        event_type:,
+        event_schema:,
         aggregate_id:,
         data:,
         occurred_at:,
-        metadata:,
-        version:
+        metadata:
       )
     end
 
@@ -22,23 +21,60 @@ RSpec.describe EventService do
     let(:version) { 4 }
     let(:id) { SecureRandom.uuid }
 
-    it "enqueues a BroadcastEvent job, persists and event and returns the produced event message" do
-      expect(BroadcastEventJob)
-        .to receive(:perform_later)
-        .with(
-          Events::Message.new(
-            id:,
-            event_type:,
-            aggregate_id:,
-            data:,
-            occurred_at:,
-            metadata:,
-            version:
-          )
-        )
+    context "when the event_schema is not a Events::Schema" do
+      let(:event_schema) { 10 }
 
-      expect { subject }.to change(Event, :count).by(1)
-      expect(subject).to eq(Event.last_created.message)
+      it "raises a NotEventSchemaError" do
+        expect { subject }.to raise_error(described_class::NotEventSchemaError)
+      end
+    end
+
+    context "when event_schema is a Events::Schema" do
+      let(:event_schema) do
+        Events::Schema.new(
+          data: Hash,
+          metadata: Hash,
+          event_type:,
+          version:
+        )
+      end
+
+      context "when data doesn't type check" do
+        let(:data) { "cat" }
+
+        it "raies a InvalidSchemaError" do
+          expect { subject }.to raise_error(described_class::InvalidSchemaError)
+        end
+      end
+
+      context "when metadata doesn't type check" do
+        let(:metadata) { [1, 2, 3] }
+
+        it "raies a InvalidSchemaError" do
+          expect { subject }.to raise_error(described_class::InvalidSchemaError)
+        end
+      end
+
+      context "when data and metadata type check" do
+        it "enqueues a BroadcastEvent job, persists and event and returns the produced event message" do
+          expect(BroadcastEventJob)
+            .to receive(:perform_later)
+            .with(
+              Events::Message.new(
+                id:,
+                event_type:,
+                aggregate_id:,
+                data:,
+                occurred_at:,
+                metadata:,
+                version:
+              )
+            )
+
+          expect { subject }.to change(Event, :count).by(1)
+          expect(subject).to eq(Event.last_created.message)
+        end
+      end
     end
   end
 end

5308430bad276f19113549d5fd43928d4ed87117
John Chabot <johnchabot2013@gmail.com>
2024-01-26 11:43:32 -0500

    Move event message inside of Events module

--- spec/models/events/message_spec.rb
+++ spec/models/events/message_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe EventMessage do
+RSpec.describe Events::Message do
   describe "#initialize" do
     context "when missing a required key" do
       it "raises a ValueSemantics::MissingAttributes error" do
@@ -36,28 +36,28 @@ RSpec.describe EventMessage do
     describe "#occurred_at" do
       context "when passed a Time for occurred_at" do
         it "converts it to ActiveSupport::TimeWithZone" do
-          message = build(:event_message, occurred_at: Time.new(2000, 1, 1, 0, 0, 0, "+00:00"))
+          message = build(:events__message, occurred_at: Time.new(2000, 1, 1, 0, 0, 0, "+00:00"))
           expect(message.occurred_at).to eq(Time.zone.parse('2000-01-01 00:00:00 UTC'))
         end
       end
 
       context "when passed a DateTime for occurred_at" do
         it "converts it to ActiveSupport::TimeWithZone" do
-          message = build(:event_message, occurred_at: DateTime.new(2000, 1, 1, 0, 0, 0))
+          message = build(:events__message, occurred_at: DateTime.new(2000, 1, 1, 0, 0, 0))
           expect(message.occurred_at).to eq(Time.zone.parse('2000-01-01 00:00:00 UTC'))
         end
       end
 
       context "when passed a formatted string string for occurred_at" do
         it "converts it to ActiveSupport::TimeWithZone" do
-          message = build(:event_message, occurred_at: "2000-01-01 00:00:00 UTC")
+          message = build(:events__message, occurred_at: "2000-01-01 00:00:00 UTC")
           expect(message.occurred_at).to eq(Time.zone.parse('2000-01-01 00:00:00 UTC'))
         end
       end
 
       context "when passed something else" do
         it "converts it to ActiveSupport::TimeWithZone" do
-          expect { build(:event_message, occurred_at: "Cat o'clock") }.to raise_error(ValueSemantics::InvalidValue)
+          expect { build(:events__message, occurred_at: "Cat o'clock") }.to raise_error(ValueSemantics::InvalidValue)
         end
       end
     end

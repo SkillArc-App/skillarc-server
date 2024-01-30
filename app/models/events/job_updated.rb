8c8db6241a0f9d2f4c4793ccf6e30143bbf416fa
John Chabot <johnchabot2013@gmail.com>
2024-01-30 12:42:34 -0500

    Fix mistyped event schema

--- app/models/events/job_updated.rb
+++ app/models/events/job_updated.rb
@@ -3,7 +3,7 @@ module Events
     V1 = Schema.build(
       data: Common::UntypedHashWrapper,
       metadata: Common::Nothing,
-      event_type: Event::EventTypes::COACH_ASSIGNED,
+      event_type: Event::EventTypes::JOB_UPDATED,
       version: 1
     )
   end

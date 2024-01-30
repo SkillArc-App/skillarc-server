79d885c8d4d0db4e5880cbfb96d499c3f8bbb54e
John Chabot <johnchabot2013@gmail.com>
2024-01-30 16:16:23 -0500

    Be good with rspec

--- spec/services/pubsub_spec.rb
+++ spec/services/pubsub_spec.rb
@@ -5,11 +5,10 @@ RSpec.describe Pubsub do
 
     subscriber = double("subscriber")
     allow(subscriber).to receive(:call)
-    pubsub = described_class.new
-    pubsub.subscribe(event_schema: event.event_schema, subscriber:)
+    subject.subscribe(event_schema: event.event_schema, subscriber:)
 
     # Act
-    pubsub.publish(event:)
+    subject.publish(event:)
 
     # Assert
     expect(subscriber).to have_received(:call).with(event:)

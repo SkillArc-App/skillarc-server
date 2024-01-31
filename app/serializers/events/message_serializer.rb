3fa57d532576c9cd6c092d09aa04377deadc6a21
Christopher Brauns <chris@blocktrainapp.com>
2024-01-31 11:00:28 -0500

    symbolize the deserialization

--- app/serializers/events/message_serializer.rb
+++ app/serializers/events/message_serializer.rb
@@ -23,8 +23,8 @@ module Events
         id: hash["id"],
         aggregate_id: hash["aggregate_id"],
         event_type: hash["event_type"],
-        data: schema.data.from_hash(hash["data"]),
-        metadata: schema.metadata.from_hash(hash["metadata"]),
+        data: schema.data.from_hash(hash["data"].symbolize_keys),
+        metadata: schema.metadata.from_hash(hash["metadata"].symbolize_keys),
         version: hash["version"],
         occurred_at: Time.zone.parse(hash["occurred_at"])
       )

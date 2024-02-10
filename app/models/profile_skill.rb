b555e44ccb3a2c525d5522de6896e965e9fd4c5d
John Chabot <johnchabot2013@gmail.com>
2024-02-10 11:07:25 -0500

    Drop profile_id on profile_skills

--- app/models/profile_skill.rb
+++ app/models/profile_skill.rb
@@ -7,7 +7,6 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  master_skill_id :text             not null
-#  profile_id      :text             not null
 #  seeker_id       :uuid             not null
 #
 # Indexes
@@ -17,11 +16,10 @@
 # Foreign Keys
 #
 #  ProfileSkill_master_skill_id_fkey  (master_skill_id => master_skills.id) ON DELETE => restrict ON UPDATE => cascade
-#  ProfileSkill_profile_id_fkey       (profile_id => profiles.id) ON DELETE => restrict ON UPDATE => cascade
 #  fk_rails_...                       (seeker_id => seekers.id)
 #
 class ProfileSkill < ApplicationRecord
+  self.ignored_columns = ["profile_id"]
   belongs_to :master_skill
-  belongs_to :profile
   belongs_to :seeker
 end

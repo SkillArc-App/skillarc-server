8f8d345c65f6f6dbdd77237a214c3429594d22e1
Christopher Brauns <chris@blocktrainapp.com>
2024-02-14 15:23:33 -0500

    Allow optionally no side effects

--- app/services/employers/application_notification_service.rb
+++ app/services/employers/application_notification_service.rb
@@ -6,24 +6,21 @@ module Employers
       ].freeze
     end
 
-    def self.handle_event(message, *_params)
+    def self.handle_event(message, with_side_effects: true)
       case message.event_schema
       when Events::ApplicantStatusUpdated::V2
-        handle_applicant_status_updated(message)
+        handle_applicant_status_updated(message, with_side_effects:)
       end
     end
 
     def self.reset_for_replay
       Applicant.destroy_all
-      Employer.destroy_all
-      Job.destroy_all
-      Recruiter.destroy_all
     end
 
     class << self
       private
 
-      def handle_applicant_status_updated(message)
+      def handle_applicant_status_updated(message, with_side_effects: true)
         job = Job.find_by(job_id: message.data.job_id)
         applicant = Applicant.find_or_initialize_by(
           applicant_id: message.data.applicant_id,
@@ -39,6 +36,7 @@ module Employers
           status: message.data.status
         )
 
+        return unless with_side_effects
         return unless applicant.status == Applicant::StatusTypes::NEW
 
         Contact::SmtpService.new.notify_employer_of_applicant(

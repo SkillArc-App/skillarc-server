2f6994abf4973489e9902738bd847e9bc7343188
John Chabot <johnchabot2013@gmail.com>
2024-02-15 14:02:51 -0500

    Add new V3 applicant status

--- app/models/events/applicant_status_updated.rb
+++ app/models/events/applicant_status_updated.rb
@@ -1,5 +1,16 @@
 module Events
   module ApplicantStatusUpdated
+    module Reason
+      class V1
+        extend Concerns::Payload
+
+        schema do
+          id Uuid
+          answer String
+        end
+      end
+    end
+
     module Data
       class V2
         extend Concerns::Payload
@@ -19,6 +30,26 @@ module Events
           status Either(*ApplicantStatus::StatusTypes::ALL)
         end
       end
+
+      class V3
+        extend Concerns::Payload
+
+        schema do
+          applicant_id Uuid
+          applicant_first_name String
+          applicant_last_name String
+          applicant_email String
+          applicant_phone_number Either(String, nil), default: nil
+          profile_id Either(Uuid, nil), default: nil
+          seeker_id Uuid
+          user_id String
+          job_id Uuid
+          employer_name String
+          employment_title String
+          status Either(*ApplicantStatus::StatusTypes::ALL)
+          reason ArrayOf(Reason::V1)
+        end
+      end
     end
 
     V2 = Schema.build(
@@ -26,6 +57,12 @@ module Events
       metadata: Common::Nothing,
       event_type: Event::EventTypes::APPLICANT_STATUS_UPDATED,
       version: 2
+    )
+    V3 = Schema.build(
+      data: Data::V3,
+      metadata: Common::Nothing,
+      event_type: Event::EventTypes::APPLICANT_STATUS_UPDATED,
+      version: 3
     )
   end
 end

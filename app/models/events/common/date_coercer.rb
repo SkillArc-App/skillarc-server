5f558693a4e4e7518cff03a359bd4f1ce8546d8e
John Chabot <johnchabot2013@gmail.com>
2024-02-15 12:24:43 -0500

    Update date coercer to handle different formats

--- app/models/events/common/date_coercer.rb
+++ app/models/events/common/date_coercer.rb
@@ -5,7 +5,11 @@ module Events
         if value.is_a?(DateTime) || value.is_a?(ActiveSupport::TimeWithZone) || value.is_a?(Time)
           value.to_date
         elsif value.is_a?(String)
-          Date.parse(value)
+          if %r{\d{2}/\d{2}/\d{4}}.match(value)
+            Date.strptime(value, "%m/%d/%Y")
+          else
+            Date.strptime(value, "%Y-%m-%d")
+          end
         else
           value
         end

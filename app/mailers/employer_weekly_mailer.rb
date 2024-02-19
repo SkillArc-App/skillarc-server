2d65384f15472bb3e2eab7fd3dd870f02b400238
Christopher Brauns <chris@blocktrainapp.com>
2024-02-19 16:13:28 -0500

    Add weekly mailer

---
+++ app/mailers/employer_weekly_mailer.rb
@@ -0,0 +1,18 @@
+class EmployerWeeklyMailer < ApplicationMailer
+  default from: 'admin@skillarc.com'
+
+  def applicants
+    @employer = params[:employer]
+    @recruiter = params[:recruiter]
+
+    @job = params[:job]
+
+    @new_applicants = params[:new_applicants]
+    @pending_applicants = params[:pending_applicants]
+
+    mail(
+      to: @recruiter.email,
+      subject: 'Weekly Applicant Summary' # rubocop:disable Rails/I18nLocaleTexts
+    )
+  end
+end

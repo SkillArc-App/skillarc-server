99d2ee4f4f7b74d583f361392a4dcc3bfbe90b90
John Chabot <johnchabot2013@gmail.com>
2024-02-20 11:59:18 -0500

    Add additional shared examples for openapi

--- spec/shared_examples/admin_secure_endpoint.rb
+++ spec/shared_examples/admin_secure_endpoint.rb
@@ -42,3 +42,36 @@ RSpec.shared_examples "admin secured endpoint" do
     end
   end
 end
+
+RSpec.shared_context "admin authenticated openapi" do
+  let!(:user) do
+    u = User.find_or_create_by!(
+      id: '1a7d78bd-ae41-4d09-95d5-0b417efbcb7f',
+      first_name: 'Jake',
+      last_name: 'Not-Onboard',
+      email: 'jake@statefarm.com',
+      sub: 'jakesub'
+    )
+    UserRole.create!(id: SecureRandom.uuid, user: u, role:)
+    u
+  end
+  let(:role) { Role.create!(id: SecureRandom.uuid, name: "admin") }
+  let(:Authorization) { "Bearer #{user.sub}" }
+
+  around do |example|
+    original = ENV.fetch("MOCK_AUTH", nil)
+    ENV["MOCK_AUTH"] = "true"
+
+    example.run
+
+    ENV["MOCK_AUTH"] = original
+  end
+end
+
+RSpec.shared_examples "admin spec unauthenticated openapi" do
+  let(:Authorization) { nil }
+
+  response '401', 'unauthenticated' do
+    run_test!
+  end
+end
--- spec/shared_examples/coach_secure_endpoint.rb
+++ spec/shared_examples/coach_secure_endpoint.rb
@@ -54,3 +54,48 @@ RSpec.shared_examples "coach secured endpoint" do
     end
   end
 end
+
+RSpec.shared_context "coach authenticated openapi" do
+  let!(:user) do
+    role = Role.create!(
+      id: SecureRandom.uuid,
+      name: "coach"
+    )
+
+    u = User.create!(
+      id: '1a7d78bd-ae41-4d09-95d5-0b417efbcb7f',
+      first_name: 'Jake',
+      last_name: 'Not-Onboard',
+      email: 'jake@statefarm.com',
+      sub: 'jakesub'
+    )
+
+    UserRole.create!(
+      id: SecureRandom.uuid,
+      user_id: u.id,
+      role_id: role.id
+    )
+
+    u
+  end
+  let!(:coach) { create(:coaches__coach, user_id: user.id) }
+
+  let(:Authorization) { "Bearer #{user.sub}" }
+
+  around do |example|
+    original = ENV.fetch("MOCK_AUTH", nil)
+    ENV["MOCK_AUTH"] = "true"
+
+    example.run
+
+    ENV["MOCK_AUTH"] = original
+  end
+end
+
+RSpec.shared_examples "coach spec unauthenticated openapi" do
+  let(:Authorization) { nil }
+
+  response '401', 'unauthenticated' do
+    run_test!
+  end
+end

88fbb72bb1070db9a59c1180f06b41b5e3e50409
John Chabot <johnchabot2013@gmail.com>
2024-02-20 21:37:37 -0500

    Fix string array serialization edge case

--- spec/models/events/concerns/payload_spec.rb
+++ spec/models/events/concerns/payload_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Events::Concerns::Payload do
 
       schema do
         child ArrayOf(Events::ApplicantStatusUpdated::Reason::V1)
+        stringChild ArrayOf(String)
         optional Either(String, Events::Common::UNDEFINED), default: Events::Common::UNDEFINED
       end
     end
@@ -15,19 +16,20 @@ RSpec.describe Events::Concerns::Payload do
   describe "#to_h" do
     it "returns all defined properties" do
       id = SecureRandom.uuid
-      instance = parent_klass.new(child: [Events::ApplicantStatusUpdated::Reason::V1.new(id:, response: nil)])
+      instance = parent_klass.new(stringChild: ['cat'], child: [Events::ApplicantStatusUpdated::Reason::V1.new(id:, response: nil)])
 
-      expect(instance.to_h).to eq({ child: [{ id:, response: nil }] })
+      expect(instance.to_h).to eq({ stringChild: ['cat'], child: [{ id:, response: nil }] })
     end
   end
 
   describe ".from_hash" do
     it "returns all defined properties" do
       id = SecureRandom.uuid
-      instance = parent_klass.from_hash({ child: [{ id:, response: nil }] })
+      instance = parent_klass.from_hash({ stringChild: ['cat'], child: [{ id:, response: nil }] })
 
       expect(instance).to eq(parent_klass.new(
                                child: [Events::ApplicantStatusUpdated::Reason::V1.new(id:, response: nil)],
+                               stringChild: ['cat'],
                                optional: Events::Common::UNDEFINED
                              ))
     end

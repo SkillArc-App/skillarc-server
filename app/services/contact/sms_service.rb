7d9da40fdaf6c74a9141ee4bda3bafa6ba7ba2a4
John Chabot <johnchabot2013@gmail.com>
2024-02-23 14:01:39 -0500

    fixup rubocop

--- app/services/contact/sms_service.rb
+++ app/services/contact/sms_service.rb
@@ -23,23 +23,21 @@ module Contact
     private
 
     def handle_send_sms(message)
-      begin
-        sms_service.send_message(
+      sms_service.send_message(
+        phone_number: message.data.phone_number,
+        message: message.data.message
+      )
+
+      EventService.create!(
+        event_schema: Events::SmsSent::V1,
+        aggregate_id: message.data.phone_number,
+        data: Events::SmsSent::Data::V1.new(
           phone_number: message.data.phone_number,
           message: message.data.message
         )
-
-        EventService.create!(
-          event_schema: Events::SmsSent::V1,
-          aggregate_id: message.data.phone_number,
-          data: Events::SmsSent::Data::V1.new(
-            phone_number: message.data.phone_number,
-            message: message.data.message
-          )
-        )
-      rescue StandardError => e
-        Sentry.capture_exception(e)
-      end
+      )
+    rescue StandardError => e
+      Sentry.capture_exception(e)
     end
 
     attr_reader :sms_service

f58680b0ec54cf092d371d13c5ed8e12daa291b5
John Chabot <johnchabot2013@gmail.com>
2024-03-05 15:04:51 -0500

    Add on_message helper for message consumers

--- app/lib/message_consumer.rb
+++ app/lib/message_consumer.rb
@@ -1,21 +1,57 @@
 class MessageConsumer
+  NotSchemaError = Class.new(StandardError)
+  NotValidSubscriberType = Class.new(StandardError)
+
+  def call(message:)
+    handle_message(message)
+  end
+
   def handle_message(message, now: Time.zone.now) # rubocop:disable Lint/UnusedMethodArgument
-    raise NoMethodError
+    schema = message.schema
+    send("#{schema.message_type}_#{schema.version}", message)
   end
 
   def all_handled_messages
     handled_messages + handled_messages_sync
   end
 
+  def self.on_message(schema, subscribe_type = :async, &)
+    raise NotSchemaError unless schema.is_a?(Messages::Schema)
+    raise NotValidSubscriberType unless subscribe_type.in?(%i[async sync])
+
+    if subscribe_type == :sync
+      handled_messages_sync << schema
+    else
+      handled_messages << schema
+    end
+
+    method_name = "#{schema.message_type}_#{schema.version}".to_sym
+    define_method(method_name, &)
+
+    private method_name
+  end
+
   def handled_messages
-    []
+    self.class.handled_messages
   end
 
   def handled_messages_sync
-    []
+    self.class.handled_messages_sync
   end
 
   def reset_for_replay
     raise NoMethodError
+  end
+
+  def self.handled_messages
+    @handled_messages ||= []
+  end
+
+  def self.handled_messages_sync
+    @handled_messages_sync ||= []
+  end
+
+  def self.handlers
+    @handlers ||= {}
   end
 end

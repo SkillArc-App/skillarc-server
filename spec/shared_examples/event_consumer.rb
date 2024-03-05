2d849c65977fd09cc0c624fcea965efe489acaaf
John Chabot <johnchabot2013@gmail.com>
2024-03-05 15:05:33 -0500

    Remove unneed spec from shared example

--- spec/shared_examples/event_consumer.rb
+++ spec/shared_examples/event_consumer.rb
@@ -1,14 +1,4 @@
 RSpec.shared_examples "an event consumer" do
-  describe ".handle_message" do
-    subject { described_class.new.handle_message(message) }
-
-    let(:message) { build(:message, :role_added) }
-
-    it "does not raise an error" do
-      expect { subject }.not_to raise_error
-    end
-  end
-
   describe ".reset_for_replay" do
     subject { described_class.new.reset_for_replay }
 

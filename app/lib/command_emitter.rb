46897e992857bd7aaad0fcbb3f186b80de56b0e4
Christopher Brauns <chris@blocktrainapp.com>
2024-03-14 11:11:07 -0400

    Add command emitter

--- app/lib/command_emitter.rb
+++ app/lib/command_emitter.rb
@@ -1,7 +1,16 @@
 module CommandEmitter
   private
 
+  def with_command_service(command_service = CommandService.new)
+    Thread.current.thread_variable_set(:command_service, command_service)
+    yield
+  ensure
+    command_service.flush
+
+    Thread.current.thread_variable_set(:command_service, nil)
+  end
+
   def command_service
-    @command_service ||= CommandService.new
+    Thread.current.thread_variable_get(:command_service)
   end
 end

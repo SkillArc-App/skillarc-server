1f25327ec116e416552cec8e26cc622bb849999f
John Chabot <johnchabot2013@gmail.com>
2024-04-01 17:20:50 -0400

    Re create play

---
+++ spec/jobs/play_stream_job_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe PlayStreamJob do
+  let(:listener) { DbStreamAggregator.build(consumer:, listener_name:) }
+  let(:consumer) { double(:consumer, handle_message: nil, :event_service= => nil, :command_service= => nil) }
+  let(:listener_name) { SecureRandom.uuid }
+
+  it "plays the stream" do
+    expect(listener)
+      .to receive(:play)
+
+    described_class.new.perform(listener_name:)
+  end
+end

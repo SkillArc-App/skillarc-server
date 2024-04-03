267e7083d3e818060aba51819c166935be41d987
John Chabot <johnchabot2013@gmail.com>
2024-04-03 11:26:47 -0400

    Add logging on app controller not found response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound do |_|
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    Rails.logger.debug e
     render json: { error: 'Resource not found' }, status: :not_found
   end
 end

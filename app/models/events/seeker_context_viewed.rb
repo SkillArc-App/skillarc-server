fee096b8b8aef740216c04a1e985df010775d294
John Chabot <johnchabot2013@gmail.com>
2024-04-09 09:49:51 -0400

    Add seeker context viewed

---
+++ app/models/events/seeker_context_viewed.rb
@@ -0,0 +1,21 @@
+module Events
+  module SeekerContextViewed
+    module Data
+      class V1
+        extend Messages::Payload
+
+        schema do
+          context_id Uuid
+        end
+      end
+    end
+
+    V1 = Messages::Schema.active(
+      data: Data::V1,
+      metadata: Messages::Nothing,
+      aggregate: Aggregates::Coach,
+      message_type: Messages::Types::Seekers::SEEKER_CONTEXT_VIEWED,
+      version: 1
+    )
+  end
+end

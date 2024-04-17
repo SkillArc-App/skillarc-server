6f4280ae6a3a9f956b5eb4ba3699b0bb44be9346
John Chabot <johnchabot2013@gmail.com>
2024-04-17 14:28:04 -0400

    rubocop

--- db/migrate/20240417180303_drop_message_state.rb
+++ db/migrate/20240417180303_drop_message_state.rb
@@ -1,5 +1,5 @@
 class DropMessageState < ActiveRecord::Migration[7.1]
   def change
-    drop_table :contact_message_states
+    drop_table :contact_message_states # rubocop:disable Rails/ReversibleMigration
   end
 end

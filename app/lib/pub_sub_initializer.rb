642e25b44d7c85bc1c7ee20d8c6787ec890c9373
John Chabot <johnchabot2013@gmail.com>
2024-04-17 10:49:12 -0400

    Separate aggregators and reactors in init

--- app/lib/pub_sub_initializer.rb
+++ app/lib/pub_sub_initializer.rb
@@ -48,24 +48,29 @@ module PubSubInitializer
       subscriber: Klayvio::ChatMessageReceived.new
     )
 
-    [
+    aggregators = [
       DbStreamAggregator.build(consumer: Analytics::AnalyticsAggregator.new, listener_name: "analytics"),
       DbStreamAggregator.build(consumer: Coaches::CoachesAggregator.new, listener_name: "coach_seekers"),
+      DbStreamAggregator.build(consumer: Search::SearchService.new, listener_name: "search"),
+      DbStreamAggregator.build(consumer: Contact::ContactAggregator.new, listener_name: "contact_aggregator"),
+      DbStreamAggregator.build(consumer: Infrastructure::InfrastructureAggregator.new, listener_name: "infrastructure_aggregator"),
+      DbStreamAggregator.build(consumer: Employers::EmployerAggregator.new, listener_name: "employers"),
+      DbStreamAggregator.build(consumer: Seekers::SeekerService.new, listener_name: "seekers")
+    ]
+
+    reactors = [
       DbStreamReactor.build(consumer: Coaches::CoachesReactor.new, listener_name: "coach_seekers_reactor"),
       DbStreamReactor.build(consumer: Applicants::OrchestrationReactor.new, listener_name: "applicants_orchestration_reactor"),
       DbStreamReactor.build(consumer: Slack::SlackReactor.new, listener_name: "slack_reactor"),
-      DbStreamAggregator.build(consumer: Search::SearchService.new, listener_name: "search"),
-      DbStreamAggregator.build(consumer: Contact::ContactAggregator.new, listener_name: "contact_aggregator"),
       DbStreamReactor.build(consumer: Contact::ContactReactor.new, listener_name: "infrastructure_reactor"),
       DbStreamReactor.build(consumer: Infrastructure::InfrastructureReactor.new, listener_name: "infrastructure_reactor"),
-      DbStreamAggregator.build(consumer: Infrastructure::InfrastructureAggregator.new, listener_name: "infrastructure_aggregator"),
       DbStreamReactor.build(consumer: Contact::SmsReactor.new, listener_name: "contact_sms"),
       DbStreamReactor.build(consumer: Contact::SmtpReactor.new, listener_name: "contact_smtp"),
       DbStreamReactor.build(consumer: Contact::CalDotCom::SchedulingReactor.new, listener_name: "cal_com_scheduling"),
-      DbStreamAggregator.build(consumer: Employers::EmployerAggregator.new, listener_name: "employers"),
-      DbStreamReactor.build(consumer: Employers::EmployerReactor.new, listener_name: "employers_application_notification_service"),
-      DbStreamAggregator.build(consumer: Seekers::SeekerService.new, listener_name: "seekers")
-    ].each do |listener|
+      DbStreamReactor.build(consumer: Employers::EmployerReactor.new, listener_name: "employers_application_notification_service")
+    ]
+
+    (aggregators + reactors).each do |listener|
       listener.handled_messages_sync.each do |message_schema|
         PUBSUB_SYNC.subscribe(
           message_schema:,

6470d2d6c49e8cc8d65f9f977ef2c6bb96ee7821
Christopher Brauns <chris@blocktrainapp.com>
2024-04-23 17:29:09 -0400

    add types to attribute events

--- app/models/events/attribute_deleted.rb
+++ app/models/events/attribute_deleted.rb
@@ -1,6 +1,7 @@
 module Events
   module AttributeDeleted
     V1 = Messages::Schema.active(
+      type: Messages::EVENT,
       data: Messages::Nothing,
       metadata: Messages::Nothing,
       aggregate: Aggregates::Attribute,

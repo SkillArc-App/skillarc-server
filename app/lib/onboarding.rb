837fbd2c10400f70ce20d4b9f36e7becea87da0a
John Chabot <johnchabot2013@gmail.com>
2024-04-30 14:42:12 -0400

    Update onboarding library to just emit messages

--- app/lib/onboarding.rb
+++ app/lib/onboarding.rb
@@ -1,232 +1,134 @@
-class Onboarding # rubocop:disable Metrics/ClassLength
-  include MessageEmitter
+class Onboarding
+  module Steps
+    ALL = [
+      START = "start".freeze,
+      NAME = 'name'.freeze,
+      RELIABILITY = 'reliability'.freeze,
+      EMPLOYMENT = 'employment'.freeze,
+      EDUCATION = "education".freeze,
+      TRAINING = "training".freeze,
+      OPPORTUNITIES = "opportunities".freeze,
+      COMPLETE = "complete".freeze
+    ].freeze
+  end
 
-  attr_reader :onboarding_session
-
-  def initialize(onboarding_session:)
-    @onboarding_session = onboarding_session
+  def initialize(message_service:, seeker_id:, user_id:, trace_id:)
+    @seeker_id = seeker_id
+    @trace_id = trace_id
+    @user_id = user_id
+    @seeker_reactor = Seekers::SeekerReactor.new(message_service:)
   end
 
   def update(responses:)
-    add_seeker
     update_name(retrieve_response_for(responses, "name")) if response_for?(responses, "name")
+    update_reliability(retrieve_response_for(responses, "reliability")) if response_for?(responses, "reliability")
     update_experience(retrieve_response_for(responses, "experience")) if response_for?(responses, "experience")
     update_education(retrieve_response_for(responses, "education")) if response_for?(responses, "education")
     update_training_provider(retrieve_response_for(responses, "training_provider")) if response_for?(responses, "training_provider")
     update_other(retrieve_response_for(responses, "other")) if response_for?(responses, "other")
     update_interests(retrieve_response_for(responses, "opportunity_interests")) if response_for?(responses, "opportunity_interests")
-
-    onboarding_session.update!(responses:)
-
-    return unless onboarding_complete?(retrieve_response_for(responses, "reliability"), responses) && !onboarding_session.completed_at
-
-    completed_at = Time.zone.now
-
-    onboarding_session.update!(completed_at:)
-
-    message_service.create!(
-      seeker_id: user.seeker.id,
-      schema: Events::OnboardingCompleted::V2,
-      data: Messages::Nothing,
-      occurred_at: completed_at
-    )
   end
 
   private
 
-  def add_seeker
-    return if user.seeker
+  attr_reader :seeker_reactor, :user_id, :seeker_id, :trace_id
 
-    seeker = Seeker.create!(user:)
+  def update_name(name_response)
+    seeker_reactor.basic_info_added(
+      seeker_id:,
+      trace_id:,
+      user_id:,
 
-    message_service.create!(
-      user_id: user.id,
-      schema: Events::SeekerCreated::V1,
-      data: {
-        id: seeker.id,
-        user_id: user.id
-      },
-      occurred_at: seeker.created_at
+      first_name: name_response["first_name"],
+      last_name: name_response["last_name"],
+      phone_number: name_response["phone_number"],
+      date_of_birth: Date.strptime(name_response["date_of_birth"], "%m/%d/%Y")
     )
   end
 
-  def update_name(name_response)
-    unless user.first_name == name_response["first_name"] &&
-           user.last_name == name_response["last_name"] &&
-           user.phone_number == name_response["phone_number"]
-      user.update!(
-        first_name: name_response["first_name"],
-        last_name: name_response["last_name"],
-        phone_number: name_response["phone_number"]
-      )
+  def update_reliability(reliabilities)
+    seeker_reactor.reliability_added(
+      seeker_id:,
+      trace_id:,
+      reliabilities: reliabilities.map { |r| map_reliabilities(r) }
+    )
+  end
 
-      message_service.create!(
-        user_id: user.id,
-        schema: Events::UserUpdated::V1,
-        data: {
-          email: user.email,
-          first_name: user.first_name,
-          last_name: user.last_name,
-          phone_number: user.phone_number,
-          date_of_birth: Date.strptime(name_response["date_of_birth"], "%m/%d/%Y")
-        },
-        occurred_at: user.updated_at
-      )
+  def map_reliabilities(reliability)
+    case reliability
+    when "I've had or currently have a job"
+      Reliability::JOB
+    when "I've attended a Training Program"
+      Reliability::TRAINING_PROGRAM
+    when 'I have a High School Diploma / GED'
+      Reliability::EDUCATION
     end
   end
 
   def update_experience(work_responses)
-    work_responses = work_responses.map do |wr|
-      {
+    work_responses.each do |wr|
+      seeker_reactor.experience_added(
+        seeker_id:,
+        trace_id:,
+
         organization_name: wr["company"],
         position: wr["position"],
         start_date: wr["start_date"],
         end_date: wr["end_date"],
-        description: wr["description"],
         is_current: wr["current"],
-        seeker_id: user.seeker.id
-      }
-    end
-
-    work_responses.each do |d|
-      OtherExperience.find_or_initialize_by(
-        **d
-      ) do |other_experience|
-        other_experience.id = SecureRandom.uuid
-        other_experience.save!
-
-        message_service.create!(
-          seeker_id: other_experience.seeker_id,
-          schema: Events::ExperienceAdded::V1,
-          data: {
-            id: other_experience.id,
-            organization_name: other_experience.organization_name,
-            position: other_experience.position,
-            start_date: other_experience.start_date,
-            end_date: other_experience.end_date,
-            description: other_experience.description,
-            is_current: other_experience.is_current
-          }
-        )
-      end
+        description: wr["description"]
+      )
     end
   end
 
   def update_education(education_responses)
-    data = education_responses.map do |er|
-      {
+    education_responses.each do |er|
+      seeker_reactor.education_experience_added(
+        seeker_id:,
+        trace_id:,
+
         organization_name: er["org"],
         title: er["title"],
         activities: er["activities"],
         graduation_date: er["grad_year"],
-        gpa: er["gpa"],
-        seeker_id: user.seeker.id
-      }
-    end
-
-    data.each do |d|
-      EducationExperience.find_or_initialize_by(
-        **d
-      ) do |ee|
-        ee.id = SecureRandom.uuid
-        ee.save!
-
-        message_service.create!(
-          seeker_id: ee.seeker_id,
-          schema: Events::EducationExperienceAdded::V1,
-          data: {
-            id: ee.id,
-            organization_name: ee.organization_name,
-            title: ee.title,
-            activities: ee.activities,
-            graduation_date: ee.graduation_date,
-            gpa: ee.gpa
-          }
-        )
-      end
+        gpa: er["gpa"]
+      )
     end
   end
 
   def update_training_provider(tp_responses)
     tp_responses.each do |tr|
-      SeekerTrainingProvider.find_or_initialize_by(
-        user_id: user.id,
-        training_provider_id: tr
-      ) do |stp|
-        stp.id = SecureRandom.uuid
-        stp.save!
+      seeker_reactor.seeker_training_provider_added(
+        seeker_id:,
+        trace_id:,
+        user_id:,
 
-        message_service.create!(
-          seeker_id: user.seeker.id,
-          schema: Events::SeekerTrainingProviderCreated::V2,
-          data: {
-            id: stp.id,
-            training_provider_id: stp.training_provider_id
-          }
-        )
-      end
+        training_provider_id: tr
+      )
     end
   end
 
   def update_other(other_responses)
-    data = other_responses.map do |oth_r|
-      {
-        seeker_id: user.seeker.id,
+    other_responses.each do |oth_r|
+      seeker_reactor.personal_experience_added(
+        seeker_id:,
+        trace_id:,
+
         activity: oth_r["activity"],
         description: oth_r["learning"],
         start_date: oth_r["start_date"],
         end_date: oth_r["end_date"]
-      }
-    end
-
-    data.each do |d|
-      pe = PersonalExperience.find_or_initialize_by(
-        **d
-      )
-      pe.id = SecureRandom.uuid
-      pe.save!
-
-      message_service.create!(
-        seeker_id: user.seeker.id,
-        schema: Events::PersonalExperienceAdded::V1,
-        data: {
-          id: pe.id,
-          activity: pe.activity,
-          description: pe.description,
-          start_date: pe.start_date,
-          end_date: pe.end_date
-        }
       )
     end
   end
 
   def update_interests(opportunity_interests_response)
-    data = opportunity_interests_response.map do |oi|
-      {
-        seeker_id: user.seeker.id,
-        response: oi
-      }
-    end
+    seeker_reactor.professional_interests_added(
+      seeker_id:,
+      trace_id:,
 
-    data.each do |d|
-      pi = ProfessionalInterest.find_or_initialize_by(
-        **d
-      )
-      pi.id = SecureRandom.uuid
-      pi.save!
-    end
-  end
-
-  def onboarding_complete?(reliability, responses)
-    return false unless reliability
-    return false unless (responses.dig("opportunity_interests", "response")&.length || 0).positive?
-
-    reliability.all? do |r|
-      (r == "I've had or currently have a job" && response_for?(responses, "experience")) ||
-        (r == 'I have a High School Diploma / GED' && response_for?(responses, "education")) ||
-        (r == "I've attended a Training Program" && response_for?(responses, "training_provider")) ||
-        (r == "I have other experience I'd like to share" && response_for?(responses, "other"))
-    end
+      interests: opportunity_interests_response
+    )
   end
 
   def retrieve_response_for(responses, kind)
@@ -234,10 +136,6 @@ class Onboarding # rubocop:disable Metrics/ClassLength
   end
 
   def response_for?(responses, kind)
-    (responses.dig(kind, "response")&.length || 0).positive?
-  end
-
-  def user
-    @user ||= onboarding_session.user
+    !responses.dig(kind, "response").nil?
   end
 end

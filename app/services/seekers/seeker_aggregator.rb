546563ea920da00e98d7ce4dcd7342909fd6bcfa
John Chabot <johnchabot2013@gmail.com>
2024-04-30 15:53:07 -0400

    Implement seeker aggregator

---
+++ app/services/seekers/seeker_aggregator.rb
@@ -0,0 +1,116 @@
+module Seekers
+  class SeekerAggregator < MessageConsumer
+    def reset_for_replay
+      OtherExperience.delete_all
+      EducationExperience.delete_all
+      PersonalExperience.delete_all
+      SeekerTrainingProvider.delete_all
+      ProfileSkill.delete_all
+    end
+
+    on_message Events::BasicInfoAdded::V1, :sync do |message|
+      seeker = Seeker.find(message.aggregate.id)
+      user = seeker.user
+
+      user.update!(
+        first_name: message.data.first_name,
+        last_name: message.data.last_name,
+        phone_number: message.data.phone_number
+      )
+    end
+
+    # Job experience
+    on_message Events::ExperienceAdded::V1, :sync do |message|
+      other_expereince = OtherExperience.find_or_initialize_by(id: message.data.id)
+
+      other_expereince.update!(
+        seeker_id: message.aggregate.id,
+        organization_name: message.data.organization_name,
+        position: message.data.position,
+        start_date: message.data.start_date,
+        end_date: message.data.end_date,
+        description: message.data.description,
+        is_current: message.data.is_current
+      )
+    end
+
+    on_message Events::ExperienceRemoved::V1, :sync do |message|
+      OtherExperience.find(message.data.id).destroy!
+    end
+
+    # Ed experience
+    on_message Events::EducationExperienceAdded::V1, :sync do |message|
+      education_experience = EducationExperience.find_or_initialize_by(id: message.data.id)
+
+      education_experience.update!(
+        seeker_id: message.aggregate.id,
+        id: message.data.id,
+        organization_name: message.data.organization_name,
+        title: message.data.title,
+        activities: message.data.activities,
+        graduation_date: message.data.graduation_date,
+        gpa: message.data.gpa
+      )
+    end
+
+    on_message Events::EducationExperienceDeleted::V1, :sync do |message|
+      EducationExperience.find(message.data.id).destroy!
+    end
+
+    # Personal experience
+    on_message Events::PersonalExperienceAdded::V1, :sync do |message|
+      personal_experience = PersonalExperience.find_or_initialize_by(id: message.data.id)
+
+      personal_experience.update!(
+        seeker_id: message.aggregate.id,
+        activity: message.data.activity,
+        description: message.data.description,
+        start_date: message.data.start_date,
+        end_date: message.data.end_date
+      )
+    end
+
+    on_message Events::PersonalExperienceRemoved::V1, :sync do |message|
+      PersonalExperience.find(message.data.id).destroy!
+    end
+
+    on_message Events::SeekerTrainingProviderCreated::V3, :sync do |message|
+      seeker_training_provider_created = SeekerTrainingProvider.find_or_initialize_by(id: message.data.id)
+
+      seeker_training_provider_created.update!(
+        program_id: message.data.program_id,
+        training_provider_id: message.data.training_provider_id,
+        user_id: message.data.user_id
+      )
+    end
+
+    on_message Events::OnboardingStarted::V1, :sync do |message|
+      OnboardingSession.create!(
+        id: SecureRandom.uuid,
+        user_id: message.data.user_id,
+        seeker_id: message.aggregate.id,
+        started_at: message.occurred_at
+      )
+    end
+
+    on_message Events::OnboardingCompleted::V2, :sync do |message|
+      onboarding_session = OnboardingSession.find_by(seeker_id: message.aggregate.id)
+
+      if onboarding_session.present?
+        onboarding_session.update!(
+          completed_at: message.occurred_at
+        )
+      else
+        user = Seeker.find(message.aggregate.id).user
+
+        OnboardingSession.create!(
+          id: SecureRandom.uuid,
+          user_id: user.id,
+          seeker_id: message.aggregate.id,
+          started_at: message.occurred_at,
+          completed_at: message.occurred_at
+        )
+      end
+    end
+  end
+end
---
+++ spec/services/seekers/seeker_aggregator_spec.rb
@@ -0,0 +1,313 @@
+require 'rails_helper'
+
+RSpec.describe Seekers::SeekerAggregator do
+  it_behaves_like "a message consumer"
+
+  let(:consumer) { described_class.new(message_service:) }
+  let(:message_service) { MessageService.new }
+
+  let(:user) { create(:user, onboarding_session: nil) }
+  let(:seeker) { create(:seeker, user:) }
+
+  describe "#handle_message" do
+    subject { consumer.handle_message(message) }
+
+    context "when the message is basic info added" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::BasicInfoAdded::V1,
+          aggregate_id: seeker.id,
+          data: {
+            user_id: user.id,
+            first_name: "John",
+            last_name: "Chabot",
+            phone_number: "333-333-3333",
+            date_of_birth: "10-10-2000"
+          }
+        )
+      end
+
+      it "creates a onboarding session" do
+        subject
+
+        user.reload
+        expect(user.first_name).to eq(message.data.first_name)
+        expect(user.last_name).to eq(message.data.last_name)
+        expect(user.phone_number).to eq(message.data.phone_number)
+      end
+    end
+
+    context "when the message is experience added" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::ExperienceAdded::V1,
+          aggregate_id: seeker.id,
+          data: {
+            id: SecureRandom.uuid,
+            organization_name: "Org",
+            position: "Position",
+            start_date: "A",
+            end_date: "$$$",
+            description: "{}",
+            is_current: false
+          }
+        )
+      end
+
+      context "when the event updates a record" do
+        let!(:other_experience) { create(:other_experience, seeker_id: message.aggregate.id, id: message.data.id) }
+
+        it "updates an other session" do
+          expect { subject }.not_to change(OtherExperience, :count)
+
+          other_experience.reload
+          expect(other_experience.id).to eq(message.data.id)
+          expect(other_experience.seeker_id).to eq(message.aggregate.id)
+          expect(other_experience.organization_name).to eq(message.data.organization_name)
+          expect(other_experience.position).to eq(message.data.position)
+          expect(other_experience.start_date).to eq(message.data.start_date)
+          expect(other_experience.end_date).to eq(message.data.end_date)
+          expect(other_experience.description).to eq(message.data.description)
+          expect(other_experience.is_current).to eq(message.data.is_current)
+        end
+      end
+
+      context "when the event creates a record" do
+        it "creates an other experience" do
+          expect { subject }.to change(OtherExperience, :count).from(0).to(1)
+
+          other_experience = OtherExperience.take(1).first
+          expect(other_experience.id).to eq(message.data.id)
+          expect(other_experience.seeker_id).to eq(message.aggregate.id)
+          expect(other_experience.organization_name).to eq(message.data.organization_name)
+          expect(other_experience.position).to eq(message.data.position)
+          expect(other_experience.start_date).to eq(message.data.start_date)
+          expect(other_experience.end_date).to eq(message.data.end_date)
+          expect(other_experience.description).to eq(message.data.description)
+          expect(other_experience.is_current).to eq(message.data.is_current)
+        end
+      end
+    end
+
+    context "when the message is experience removed" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::ExperienceRemoved::V1,
+          aggregate_id: seeker.id,
+          data: {
+            id:
+          }
+        )
+      end
+      let(:id) { SecureRandom.uuid }
+      let!(:other_experience) { create(:other_experience, seeker_id: seeker.id, id:) }
+
+      it "remove the other experience" do
+        expect { subject }.to change(OtherExperience, :count).from(1).to(0)
+      end
+    end
+
+    context "when the message is education experience added" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::EducationExperienceAdded::V1,
+          aggregate_id: seeker.id,
+          data: {
+            id: SecureRandom.uuid,
+            organization_name: "Org",
+            title: "Position",
+            activities: "A",
+            graduation_date: "$$$",
+            gpa: "{}"
+          }
+        )
+      end
+
+      context "when the event updates a record" do
+        let!(:education_experience) { create(:education_experience, seeker_id: message.aggregate.id, id: message.data.id) }
+
+        it "updates and education experience" do
+          expect { subject }.not_to change(EducationExperience, :count)
+
+          education_experience.reload
+          expect(education_experience.id).to eq(message.data.id)
+          expect(education_experience.seeker_id).to eq(seeker.id)
+          expect(education_experience.organization_name).to eq(message.data.organization_name)
+          expect(education_experience.title).to eq(message.data.title)
+          expect(education_experience.activities).to eq(message.data.activities)
+          expect(education_experience.graduation_date).to eq(message.data.graduation_date)
+          expect(education_experience.gpa).to eq(message.data.gpa)
+        end
+      end
+
+      context "when the event creates a record" do
+        it "creates a education experience" do
+          expect { subject }.to change(EducationExperience, :count).from(0).to(1)
+
+          education_experience = EducationExperience.take(1).first
+          expect(education_experience.id).to eq(message.data.id)
+          expect(education_experience.seeker_id).to eq(seeker.id)
+          expect(education_experience.organization_name).to eq(message.data.organization_name)
+          expect(education_experience.title).to eq(message.data.title)
+          expect(education_experience.activities).to eq(message.data.activities)
+          expect(education_experience.graduation_date).to eq(message.data.graduation_date)
+          expect(education_experience.gpa).to eq(message.data.gpa)
+        end
+      end
+    end
+
+    context "when the message is education experience removed" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::EducationExperienceDeleted::V1,
+          aggregate_id: seeker.id,
+          data: {
+            id:
+          }
+        )
+      end
+      let(:id) { SecureRandom.uuid }
+      let!(:education_experience) { create(:education_experience, seeker_id: seeker.id, id:) }
+
+      it "remove the education experience" do
+        expect { subject }.to change(EducationExperience, :count).from(1).to(0)
+      end
+    end
+
+    context "when the message is personal experience added" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::PersonalExperienceAdded::V1,
+          aggregate_id: seeker.id,
+          data: {
+            id: SecureRandom.uuid,
+            activity: "Org",
+            description: "Position",
+            start_date: "A",
+            end_date: "$$$"
+          }
+        )
+      end
+
+      context "when the event updates a record" do
+        let!(:personal_experience) { create(:personal_experience, seeker_id: message.aggregate.id, id: message.data.id) }
+
+        it "updates and personal experience" do
+          expect { subject }.not_to change(PersonalExperience, :count)
+
+          personal_experience.reload
+          expect(personal_experience.id).to eq(message.data.id)
+          expect(personal_experience.seeker_id).to eq(seeker.id)
+          expect(personal_experience.activity).to eq(message.data.activity)
+          expect(personal_experience.description).to eq(message.data.description)
+          expect(personal_experience.start_date).to eq(message.data.start_date)
+          expect(personal_experience.end_date).to eq(message.data.end_date)
+        end
+      end
+
+      context "when the event creates a record" do
+        it "creates a personal experience" do
+          expect { subject }.to change(PersonalExperience, :count).from(0).to(1)
+
+          personal_experience = PersonalExperience.take(1).first
+          expect(personal_experience.id).to eq(message.data.id)
+          expect(personal_experience.seeker_id).to eq(seeker.id)
+          expect(personal_experience.activity).to eq(message.data.activity)
+          expect(personal_experience.description).to eq(message.data.description)
+          expect(personal_experience.start_date).to eq(message.data.start_date)
+          expect(personal_experience.end_date).to eq(message.data.end_date)
+        end
+      end
+    end
+
+    context "when the message is personal experience removed" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::PersonalExperienceRemoved::V1,
+          aggregate_id: seeker.id,
+          data: {
+            id:
+          }
+        )
+      end
+      let(:id) { SecureRandom.uuid }
+      let!(:personal_experience) { create(:personal_experience, seeker_id: seeker.id, id:) }
+
+      it "remove the personal experience" do
+        expect { subject }.to change(PersonalExperience, :count).from(1).to(0)
+      end
+    end
+
+    context "when the message is seeker training provider created" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::SeekerTrainingProviderCreated::V3,
+          aggregate_id: seeker.id,
+          data: {
+            id:
+          }
+        )
+      end
+    end
+
+    context "when the message is onboarding started" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::OnboardingStarted::V1,
+          aggregate_id: seeker.id,
+          data: {
+            user_id: user.id
+          }
+        )
+      end
+
+      it "creates a onboarding session" do
+        expect { subject }.to change(OnboardingSession, :count).from(0).to(1)
+
+        onboarding_session = OnboardingSession.last_created
+        expect(onboarding_session.user_id).to eq(message.data.user_id)
+        expect(onboarding_session.seeker_id).to eq(message.aggregate.id)
+        expect(onboarding_session.started_at).to eq(message.occurred_at)
+      end
+    end
+
+    context "when the message is onboarding completed" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::OnboardingCompleted::V2,
+          aggregate_id: seeker.id,
+          data: Messages::Nothing
+        )
+      end
+
+      context "when an onboarding session exists" do
+        let!(:onboarding_session) { create(:onboarding_session, user:, seeker_id: seeker.id) }
+
+        it "updates the completed at time" do
+          subject
+
+          expect(onboarding_session.reload.completed_at).to eq(message.occurred_at)
+        end
+      end
+
+      context "when an onboarding session doesn't exist" do
+        it "updates the completed at time" do
+          expect { subject }.to change(OnboardingSession, :count).from(0).to(1)
+
+          onboarding_session = OnboardingSession.take(1).first
+          expect(onboarding_session.completed_at).to eq(message.occurred_at)
+        end
+      end
+    end
+  end
+end

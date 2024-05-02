4cb12238a46e22e3ca0e22904fb42463e0b79c39
John Chabot <johnchabot2013@gmail.com>
2024-05-02 14:48:08 -0400

    Drop status table

--- app/models/seeker_training_provider.rb
+++ app/models/seeker_training_provider.rb
@@ -19,6 +19,4 @@ class SeekerTrainingProvider < ApplicationRecord
   belongs_to :user
   belongs_to :training_provider
   belongs_to :program, optional: true
-
-  has_many :program_statuses, class_name: 'SeekerTrainingProviderProgramStatus', dependent: :destroy
 end

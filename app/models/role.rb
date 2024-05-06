24fd56edd85869aed74a1f74e118df1ea9d350bb
John Chabot <johnchabot2013@gmail.com>
2024-05-06 11:03:27 -0400

    Add new job order admin role

--- app/models/role.rb
+++ app/models/role.rb
@@ -16,6 +16,7 @@ class Role < ApplicationRecord
     ALL = [
       ADMIN = 'admin'.freeze,
       COACH = 'coach'.freeze,
+      JOB_ORDER_ADMIN = 'job_order_admin'.freeze,
       EMPLOYER_ADMIN = 'employer_admin'.freeze
     ].freeze
   end

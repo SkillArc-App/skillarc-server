1e905b059a61dd4074ad7c61dbfabe50cc4a1670
John Chabot <johnchabot2013@gmail.com>
2024-05-06 13:58:51 -0400

    copy based better

--- app/services/klayvio/gateway.rb
+++ app/services/klayvio/gateway.rb
@@ -2,7 +2,7 @@ module Klayvio
   class Gateway
     GatewayEnvvarNotSetError = Class.new(StandardError)
 
-    def self.build(strategy: ENV.fetch("KLAYVIO_GATEWAY_STRATEGY", nil), api_key: ENV.fetch("KLAYVIO_API_KEY", nil))
+    def self.build(strategy: ENV.fetch("KLAYVIO_GATEWAY_STRATEGY", nil), api_key: ENV.fetch("KLAVIYO_API_KEY", nil))
       case strategy
       when "real"
         RealGateway.new(api_key:)

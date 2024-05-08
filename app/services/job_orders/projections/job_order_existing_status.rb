94ff175d28e5c3e19d064d0cdb3c1ebef23cc42e
John Chabot <johnchabot2013@gmail.com>
2024-05-08 15:54:52 -0400

    Create job order existing status projector

---
+++ app/services/job_orders/projections/job_order_existing_status.rb
@@ -0,0 +1,33 @@
+module JobOrders
+  module Projections
+    class JobOrderExistingStatus < Projector
+      projection_aggregator Aggregates::JobOrder
+
+      class Projection
+        extend Record
+
+        schema do
+          status Either(*JobOrders::OrderStatus::ALL, nil)
+        end
+      end
+
+      def init
+        Projection.new(
+          status: nil
+        )
+      end
+
+      on_message Events::JobOrderActivated::V1 do |_, accumulator|
+        accumulator.with(status: JobOrders::OpenStatus::OPEN)
+      end
+
+      on_message Events::JobOrderClosed::V1 do |message, accumulator|
+        accumulator.with(status: message.data.status)
+      end
+
+      on_message Events::JobOrderStalled::V1 do |message, accumulator|
+        accumulator.with(status: message.data.status)
+      end
+    end
+  end
+end
---
+++ spec/services/job_orders/projections/job_order_existing_status_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe JobOrders::Projections::JobOrderExistingStatus do
+  describe ".project" do
+    subject { described_class.project(aggregate:) }
+
+    let(:aggregate) { Aggregates::JobOrder.new(job_order_id:) }
+    let(:job_order_id) { SecureRandom.uuid }
+
+    before do
+      allow(MessageService)
+        .to receive(:aggregate_events)
+        .with(aggregate)
+        .and_return(messages)
+    end
+
+    let(:now) { Time.zone.now }
+
+    let(:job_order_activated) do
+      build(
+        :message,
+        aggregate:,
+        schema: Events::JobOrderActivated::V1,
+        data: Messages::Nothing
+      )
+    end
+    let(:job_order_stalled) do
+      build(
+        :message,
+        aggregate:,
+        schema: Events::JobOrderStalled::V1,
+        data: {
+          status: JobOrders::IdleStatus::WAITING_ON_EMPLOYER
+        }
+      )
+    end
+    let(:job_order_closed) do
+      build(
+        :message,
+        aggregate:,
+        schema: Events::JobOrderClosed::V1,
+        data: {
+          status: JobOrders::CloseStatus::FILLED
+        }
+      )
+    end
+
+    context "when the last event was activated" do
+      let(:messages) { [job_order_closed, job_order_stalled, job_order_activated] }
+
+      it "return the open status" do
+        expect(subject.status).to eq(JobOrders::OpenStatus::OPEN)
+      end
+    end
+
+    context "when the last event was stalled" do
+      let(:messages) { [job_order_closed, job_order_activated, job_order_stalled] }
+
+      it "return the open status" do
+        expect(subject.status).to eq(job_order_stalled.data.status)
+      end
+    end
+
+    context "when the last event was closed" do
+      let(:messages) { [job_order_activated, job_order_stalled, job_order_closed] }
+
+      it "return the open status" do
+        expect(subject.status).to eq(job_order_closed.data.status)
+      end
+    end
+  end
+end

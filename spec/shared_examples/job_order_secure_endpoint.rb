72e8ef101a8dd5cc21d18f59a3b240c9f91b53ff
John Chabot <johnchabot2013@gmail.com>
2024-05-13 12:50:19 -0400

    Add job order open api helper

---
+++ spec/shared_examples/job_order_secure_endpoint.rb
@@ -0,0 +1,42 @@
+RSpec.shared_context "job order authenticated" do
+  let!(:user) do
+    role = Role.create!(
+      id: SecureRandom.uuid,
+      name: Role::Types::JOB_ORDER_ADMIN
+    )
+
+    u = User.create!(
+      id: '1a7d78bd-ae41-4d09-95d5-0b417efbcb7f',
+      first_name: 'Jake',
+      last_name: 'Not-Onboard',
+      email: 'jake@statefarm.com',
+      sub: 'jakesub'
+    )
+
+    UserRole.create!(
+      id: SecureRandom.uuid,
+      user_id: u.id,
+      role_id: role.id
+    )
+
+    u
+  end
+  let(:Authorization) { "Bearer #{user.sub}" }
+
+  around do |example|
+    original = ENV.fetch("MOCK_AUTH", nil)
+    ENV["MOCK_AUTH"] = "true"
+
+    example.run
+
+    ENV["MOCK_AUTH"] = original
+  end
+end
+
+RSpec.shared_examples "an unauthenticated user" do
+  let(:Authorization) { nil }
+
+  response '401', 'unauthenticated' do
+    run_test!
+  end
+end

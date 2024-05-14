4e4dec12506a6850e04570db284b38bade8aa34e
John Chabot <johnchabot2013@gmail.com>
2024-05-14 13:11:18 -0400

    Delete some old migrations

--- db/migrate/20240422162649_drop_applicant_status_reason.rb
+++
@@ -1,5 +0,0 @@
-class DropApplicantStatusReason < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :applicant_status_reasons # rubocop:disable Rails/ReversibleMigration
-  end
-end
--- db/migrate/20240424191236_add_seeker_id_to_onboarding_session.rb
+++
@@ -1,5 +0,0 @@
-class AddSeekerIdToOnboardingSession < ActiveRecord::Migration[7.1]
-  def change
-    add_column :onboarding_sessions, :seeker_id, :uuid
-  end
-end

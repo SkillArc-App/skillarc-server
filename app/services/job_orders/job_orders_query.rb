8ee781e64085befc0c41b4fbe8fb545977bce542
John Chabot <johnchabot2013@gmail.com>
2024-05-14 17:27:30 -0400

    Add filter in query for only applicable jobs

--- app/services/job_orders/job_orders_query.rb
+++ app/services/job_orders/job_orders_query.rb
@@ -1,7 +1,7 @@
 module JobOrders
   class JobOrdersQuery
     def self.all_orders
-      JobOrder.includes(:job).all.map do |job_order|
+      JobOrder.includes(:job).where(job_orders_jobs: { applicable_for_job_orders: true }).map do |job_order|
         serialize_job_order_summary(job_order)
       end
     end
@@ -19,7 +19,7 @@ module JobOrders
           employment_title: job_order.job.employment_title,
           employer_name: job_order.job.employer_name,
           opened_at: job_order.opened_at,
-          closed_at: job_order.opened_at,
+          closed_at: job_order.closed_at,
           order_count: job_order.order_count,
           hire_count: job_order.hire_count,
           recommended_count: job_order.recommended_count,
--- spec/services/job_orders/job_orders_query_spec.rb
+++ spec/services/job_orders/job_orders_query_spec.rb
@@ -1,8 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe Coaches::CoachesQuery do
+RSpec.describe JobOrders::JobOrdersQuery do
   describe ".all_orders" do
-    # TODO
+    subject { described_class.all_orders }
+
+    let!(:job1) { create(:job_orders__job, applicable_for_job_orders: true) }
+    let!(:job2) { create(:job_orders__job, applicable_for_job_orders: false) }
+    let!(:job_order1) { create(:job_orders__job_order, job: job1) }
+    let!(:job_order2) { create(:job_orders__job_order, job: job2) }
+
+    it "returns all orders which have applicable jobs" do
+      expected_response = [
+        {
+          id: job_order1.id,
+          employment_title: job1.employment_title,
+          employer_name: job1.employer_name,
+          opened_at: job_order1.opened_at,
+          closed_at: job_order1.closed_at,
+          order_count: job_order1.order_count,
+          hire_count: job_order1.hire_count,
+          recommended_count: job_order1.recommended_count,
+          status: job_order1.status
+        }
+      ]
+
+      expect(subject).to eq(expected_response)
+    end
   end
 
   describe ".find_order" do

cfe9579c072fe3639e41e942f22d10fee1a7cae0
John Chabot <johnchabot2013@gmail.com>
2024-05-16 12:00:21 -0400

    Add note events

---
+++ app/models/events/job_order_note_added.rb
@@ -0,0 +1,24 @@
+module Events
+  module JobOrderNoteAdded
+    module Data
+      class V1
+        extend Messages::Payload
+
+        schema do
+          originator String
+          note_id Uuid
+          note String
+        end
+      end
+    end
+
+    V1 = Messages::Schema.active(
+      type: Messages::EVENT,
+      data: Data::V1,
+      metadata: Messages::Nothing,
+      aggregate: Aggregates::JobOrder,
+      message_type: Messages::Types::JobOrders::JOB_ORDER_NOTE_ADDED,
+      version: 1
+    )
+  end
+end
---
+++ app/models/events/job_order_note_modified.rb
@@ -0,0 +1,24 @@
+module Events
+  module JobOrderNoteModified
+    module Data
+      class V1
+        extend Messages::Payload
+
+        schema do
+          originator String
+          note_id Uuid
+          note String
+        end
+      end
+    end
+
+    V3 = Messages::Schema.active(
+      type: Messages::EVENT,
+      data: Data::V1,
+      metadata: Messages::Nothing,
+      aggregate: Aggregates::JobOrder,
+      message_type: Messages::Types::JobOrders::JOB_ORDER_NOTE_MODIFIED,
+      version: 1
+    )
+  end
+end

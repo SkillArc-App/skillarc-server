ab80b1ac73eab65086e631c9fb79151a6a045e70
John Chabot <johnchabot2013@gmail.com>
2024-05-20 17:20:08 -0400

    Optimize loading bookmark time

--- app/models/listener_bookmark.rb
+++ app/models/listener_bookmark.rb
@@ -2,11 +2,12 @@
 #
 # Table name: listener_bookmarks
 #
-#  id            :bigint           not null, primary key
-#  consumer_name :string           not null
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  event_id      :uuid
+#  id                :bigint           not null, primary key
+#  consumer_name     :string           not null
+#  current_timestamp :datetime
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  event_id          :uuid
 #
 class ListenerBookmark < ApplicationRecord
 end

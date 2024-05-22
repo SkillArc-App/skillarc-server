97819f98d0a10ef4061a025902f37ea0e1549f57
John Chabot <johnchabot2013@gmail.com>
2024-05-22 17:51:48 -0400

    Update chat services

--- app/services/seeker_chats.rb
+++ app/services/seeker_chats.rb
@@ -1,65 +1,55 @@
 class SeekerChats
-  include MessageEmitter
-
-  def initialize(user)
-    @user = user
+  def initialize(seeker:, message_service:)
+    @seeker = seeker
+    @message_service = message_service
   end
 
-  def send_message(applicant_id:, message:)
-    applicant_chat = ApplicantChat.find_or_create_by!(applicant_id:)
-
-    applicant_chat.messages.create!(user:, message:)
-
+  def send_message(application_id:, message:)
     message_service.create!(
-      schema: Events::ChatMessageSent::V1,
-      job_id: applicant_chat.applicant.job.id,
+      schema: Events::ChatMessageSent::V2,
+      application_id:,
       data: {
-        applicant_id: applicant_chat.applicant.id,
-        seeker_id: applicant_chat.applicant.seeker_id,
-        from_user_id: applicant_chat.applicant.seeker.user.id,
-        employer_name: applicant_chat.applicant.job.employer.name,
-        employment_title: applicant_chat.applicant.job.employment_title,
+        from_user_id: seeker.user.id,
+        from_name: "#{seeker.user.first_name} #{seeker.user.last_name}",
         message:
       }
     )
   end
 
   def get
-    ApplicantChat
-      .includes(messages: %i[user read_receipts], applicant: { seeker: :user, job: :employer })
-      .references(:messages, applicant: { seeker: :user, job: :employer })
-      .where(applicants: { seeker: { users_seekers: { id: user.id } } }).map do |applicant_chat|
-        job = applicant_chat.applicant.job
-        {
-          id: applicant_chat.applicant.id,
-          name: "#{job.employer.name} - #{job.employment_title}",
-          updatedAt: applicant_chat.messages.last_created&.created_at || applicant_chat.created_at,
-          messages: applicant_chat.messages.sort_by(&:created_at).map do |message|
-            {
-              id: message.id,
-              text: message.message,
-              isUser: message.user == user,
-              isRead: message.read_receipts.any? { |read_receipt| read_receipt.user == user },
-              sender: "#{message.user.first_name} #{message.user.last_name}"
-            }
-          end
-        }
-      end
+    Chats::ApplicantChat
+      .includes(:messages, :read_receipts)
+      .where(seeker_id: seeker.id).map do |applicant_chat|
+      read_until = applicant_chat.read_receipts.select { |r| r.user_id == seeker.user.id }.first&.read_until || Time.zone.at(0)
+
+      {
+        id: applicant_chat.application_id,
+        name: applicant_chat.title,
+        updatedAt: applicant_chat.chat_updated_at,
+        messages: applicant_chat.messages.sort_by(&:message_sent_at).map do |message|
+          {
+            id: message.id,
+            text: message.message,
+            isUser: message.user_id == seeker.user.id,
+            isRead: read_until > message.message_sent_at,
+            sender: message.from
+          }
+        end
+      }
+    end
   end
 
-  def mark_read(applicant_id:)
-    ApplicantChat
-      .includes(messages: %i[user read_receipts], applicant: { seeker: :user, job: :employer })
-      .references(:messages, applicant: { seeker: :user, job: :employer })
-      .where(applicants: { id: applicant_id })
-      .find_each do |applicant_chat|
-        applicant_chat.messages.each do |message|
-          message.read_receipts.find_or_create_by!(user:)
-        end
-      end
+  def mark_read(application_id:)
+    message_service.create!(
+      schema: Events::ChatRead::V1,
+      application_id:,
+      data: {
+        read_by_user_id: seeker.user.id
+      }
+    )
   end
 
   private
 
-  attr_reader :user
+  attr_reader :seeker, :message_service
 end

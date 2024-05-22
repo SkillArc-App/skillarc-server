857ab444be96ca0855385548ce6daedd4a7c1f63
John Chabot <johnchabot2013@gmail.com>
2024-05-22 12:10:54 -0400

    Create new version of seeker applied

--- app/models/events/seeker_applied.rb
+++ app/models/events/seeker_applied.rb
@@ -15,15 +15,39 @@ module Events
           employment_title String
         end
       end
+
+      class V2
+        extend Messages::Payload
+
+        schema do
+          application_id Uuid
+          seeker_first_name String
+          seeker_last_name String
+          seeker_email String
+          seeker_phone_number Either(String, nil), default: nil
+          user_id String
+          job_id Uuid
+          employer_name String
+          employment_title String
+        end
+      end
     end
 
-    V1 = Messages::Schema.active(
+    V1 = Messages::Schema.inactive(
       type: Messages::EVENT,
       data: Data::V1,
       metadata: Messages::Nothing,
       aggregate: Aggregates::Seeker,
       message_type: Messages::Types::Seekers::SEEKER_APPLIED,
       version: 1
+    )
+    V2 = Messages::Schema.active(
+      type: Messages::EVENT,
+      data: Data::V2,
+      metadata: Messages::Nothing,
+      aggregate: Aggregates::Seeker,
+      message_type: Messages::Types::Seekers::SEEKER_APPLIED,
+      version: 2
     )
   end
 end

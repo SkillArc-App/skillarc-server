62b9a3fab6a4104e61f8a782228a486ac562523f
John Chabot <johnchabot2013@gmail.com>
2024-05-23 09:06:51 -0400

    Rework orchestration reactor Go ahead and shortcut through our "screening"

--- app/services/applicants/orchestration_reactor.rb
+++ app/services/applicants/orchestration_reactor.rb
@@ -1,64 +1,26 @@
 module Applicants
   class OrchestrationReactor < MessageReactor
-    on_message Events::ApplicantScreened::V1, :sync do |event|
-      applicant = Applicant.find(event.aggregate.application_id)
-      seeker = applicant.seeker
-
+    on_message Events::SeekerApplied::V2, :sync do |message|
       message_service.create!(
-        application_id: applicant.id,
+        application_id: message.data.application_id,
+        trace_id: message.trace_id,
         schema: Events::ApplicantStatusUpdated::V6,
         data: {
-          applicant_first_name: seeker.first_name,
-          applicant_last_name: seeker.last_name,
-          applicant_email: seeker.email,
-          applicant_phone_number: seeker.phone_number,
-          seeker_id: seeker.id,
-          user_id: seeker.user.id,
-          job_id: applicant.job_id,
-          employer_name: applicant.job.employer.name,
-          employment_title: applicant.job.employment_title,
+          applicant_first_name: message.data.seeker_first_name,
+          applicant_last_name: message.data.seeker_last_name,
+          applicant_email: message.data.seeker_email,
+          applicant_phone_number: message.data.seeker_phone_number,
+          seeker_id: message.aggregate.id,
+          user_id: message.data.user_id,
+          job_id: message.data.job_id,
+          employer_name: message.data.employer_name,
+          employment_title: message.data.employment_title,
           status: ApplicantStatus::StatusTypes::NEW,
           reasons: []
         },
         metadata: {
-          user_id: seeker.user_id
-        },
-        trace_id: event.trace_id,
-        version: 5
-      )
-    end
-
-    on_message Commands::ScreenApplicant::V1, :sync do |event|
-      message_service.create!(
-        application_id: event.aggregate.application_id,
-        schema: Events::ApplicantScreened::V1,
-        data: Messages::Nothing,
-        metadata: Messages::Nothing,
-        trace_id: event.trace_id,
-        version: 1
-      )
-    end
-
-    on_message Events::SeekerApplied::V1, :sync do |event|
-      seeker_id = event.aggregate.seeker_id
-      job_id = event.data.job_id
-
-      applicant = Applicant.find_or_initialize_by(seeker_id:, job_id:) do |a|
-        a.id = SecureRandom.uuid
-        a.save!
-      end
-
-      ApplicantStatus.create!(
-        id: SecureRandom.uuid,
-        applicant:,
-        status: ApplicantStatus::StatusTypes::NEW
-      )
-
-      message_service.create!(
-        application_id: applicant.id,
-        trace_id: event.trace_id,
-        schema: Commands::ScreenApplicant::V1,
-        data: Messages::Nothing
+          user_id: message.data.user_id
+        }
       )
     end
   end
--- spec/services/applicants/orchestration_reactor_spec.rb
+++ spec/services/applicants/orchestration_reactor_spec.rb
@@ -7,112 +7,57 @@ RSpec.describe Applicants::OrchestrationReactor do
 
   let(:message_service) { MessageService.new }
 
-  describe "applicant screened" do
-    subject { instance.handle_message(applicant_screened) }
+  describe "#handle_message" do
+    subject { instance.handle_message(message) }
 
-    include_context "event emitter", false
-
-    let(:applicant_screened) { build(:message, schema: Events::ApplicantScreened::V1, aggregate_id: applicant.id) }
-    let(:applicant) { create(:applicant, seeker:, job:) }
-    let(:seeker) { create(:seeker, user:) }
-    let(:user) { create(:user, first_name: "David", last_name: "Helm", email: "david@skillarc.com", phone_number: "1234567890") }
-    let(:job) { create(:job, employer:, employment_title: "Welder") }
-    let(:employer) { create(:employer, name: "Skillarc") }
-
-    it "publishes an event" do
-      expect(message_service)
-        .to receive(:create!)
-        .with(
-          application_id: applicant.id,
-          schema: Events::ApplicantStatusUpdated::V6,
+    describe "when message is seeker applied" do
+      let(:message) do
+        build(
+          :message,
+          schema: Events::SeekerApplied::V2,
           data: {
-            applicant_first_name: "David",
-            applicant_last_name: "Helm",
-            applicant_email: "david@skillarc.com",
-            applicant_phone_number: "1234567890",
-            seeker_id: seeker.id,
-            user_id: user.id,
-            job_id: job.id,
+            application_id: SecureRandom.uuid,
+            seeker_first_name: "Katina",
+            seeker_last_name: "Hall",
+            seeker_email: "katina@skillarc.com",
+            seeker_phone_number: "123-456-7890",
+            user_id: SecureRandom.uuid,
+            job_id: SecureRandom.uuid,
             employer_name: "Skillarc",
-            employment_title: "Welder",
-            status: ApplicantStatus::StatusTypes::NEW,
-            reasons: []
+            employment_title: "Welder"
           },
-          metadata: {
-            user_id: user.id
-          },
-          trace_id: applicant_screened.trace_id,
-          version: 5
+          aggregate_id: seeker_id
         )
-        .and_call_original
+      end
+      let(:seeker_id) { SecureRandom.uuid }
 
-      subject
-    end
-  end
+      it "emits a applicant status updated" do
+        expect(message_service)
+          .to receive(:create!)
+          .with(
+            application_id: message.data.application_id,
+            trace_id: message.trace_id,
+            schema: Events::ApplicantStatusUpdated::V6,
+            data: {
+              applicant_first_name: message.data.seeker_first_name,
+              applicant_last_name: message.data.seeker_last_name,
+              applicant_email: message.data.seeker_email,
+              applicant_phone_number: message.data.seeker_phone_number,
+              seeker_id: message.aggregate.id,
+              user_id: message.data.user_id,
+              job_id: message.data.job_id,
+              employer_name: message.data.employer_name,
+              employment_title: message.data.employment_title,
+              status: ApplicantStatus::StatusTypes::NEW,
+              reasons: []
+            },
+            metadata: {
+              user_id: message.data.user_id
+            }
+          ).and_call_original
 
-  describe "screen applicant" do
-    subject { instance.handle_message(screen_applicant) }
-
-    include_context "event emitter", false
-
-    let(:screen_applicant) { build(:message, schema: Commands::ScreenApplicant::V1, aggregate_id: applicant.id) }
-    let(:applicant) { create(:applicant) }
-
-    it "publishes an event" do
-      expect(message_service)
-        .to receive(:create!)
-        .with(
-          application_id: applicant.id,
-          schema: Events::ApplicantScreened::V1,
-          data: Messages::Nothing,
-          metadata: Messages::Nothing,
-          trace_id: screen_applicant.trace_id,
-          version: 1
-        )
-        .and_call_original
-
-      subject
-    end
-  end
-
-  describe "seeker applied" do
-    subject { instance.handle_message(seeker_applied) }
-
-    let(:seeker_applied) { build(:message, :seeker_applied, data:, aggregate_id: seeker.id) }
-    let(:seeker) { create(:seeker) }
-    let(:data) do
-      Events::SeekerApplied::Data::V1.new(
-        seeker_first_name: "Katina",
-        seeker_last_name: "Hall",
-        seeker_email: "katina@skillarc.com",
-        seeker_phone_number: "123-456-7890",
-        seeker_id: seeker.id,
-        job_id: job.id,
-        employer_name: "Skillarc",
-        employment_title: "Welder"
-      )
-    end
-    let(:job) { create(:job, employment_title: "Welder") }
-
-    it "creates an applicant" do
-      expect { subject }
-        .to change(Applicant, :count).by(1)
-        .and change(ApplicantStatus, :count).by(1)
-
-      expect(ApplicantStatus.last_created.status).to eq(ApplicantStatus::StatusTypes::NEW)
-    end
-
-    it "requests an application screen" do
-      expect(message_service)
-        .to receive(:create!)
-        .with(
-          application_id: be_a(String),
-          trace_id: seeker_applied.trace_id,
-          schema: Commands::ScreenApplicant::V1,
-          data: Messages::Nothing
-        ).and_call_original
-
-      subject
+        subject
+      end
     end
   end
 end

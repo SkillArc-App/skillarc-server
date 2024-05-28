024e809f24bc0c584a47a77e7c586c69c598414b
John Chabot <johnchabot2013@gmail.com>
2024-05-28 11:43:19 -0400

    Add more info directly onto seeker

--- app/models/seeker.rb
+++ app/models/seeker.rb
@@ -2,21 +2,24 @@
 #
 # Table name: seekers
 #
-#  id         :uuid             not null, primary key
-#  about      :text
-#  bio        :string
-#  image      :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :text             not null
+#  id           :uuid             not null, primary key
+#  about        :text
+#  bio          :string
+#  email        :string
+#  first_name   :string
+#  image        :string
+#  last_name    :string
+#  phone_number :string
+#  zip_code     :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :text             not null
 #
 # Indexes
 #
 #  index_seekers_on_user_id  (user_id)
 #
 class Seeker < ApplicationRecord
-  belongs_to :user
-
   has_many :applicants, dependent: :destroy
   has_many :education_experiences, dependent: :destroy
   has_many :other_experiences, dependent: :destroy
@@ -26,10 +29,6 @@ class Seeker < ApplicationRecord
   has_many :stories, dependent: :destroy
   has_many :seeker_training_providers, dependent: :destroy
   has_one :onboarding_session, dependent: :destroy
-
-  def user
-    User.find(user_id)
-  end
 
   def hiring_status
     return 'Interviewing' if applicants.includes(:applicant_statuses).any? { |a| a.status.status == 'interviewing' }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_151450) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_152621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -874,6 +874,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_151450) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "about"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.string "zip_code"
     t.index ["user_id"], name: "index_seekers_on_user_id"
   end
 

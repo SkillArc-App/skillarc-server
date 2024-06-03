5dc77cf618692dd4e46485b4dbc9920d97f3421a
John Chabot <johnchabot2013@gmail.com>
2024-06-03 07:51:49 -0400

    Update cal.com scheduling

--- spec/services/contact/cal_dot_com/scheduling_reactor_spec.rb
+++ spec/services/contact/cal_dot_com/scheduling_reactor_spec.rb
@@ -26,103 +26,42 @@ RSpec.describe Contact::CalDotCom::SchedulingReactor do
         let(:cal_trigger_event_type) { Events::CalWebhookReceived::CalTriggerEventTypes::BOOKING_CREATED }
 
         context "when the payload type is CAREER_CONSULTATION" do
-          before do
-            allow(SecureRandom)
-              .to receive(:uuid)
-              .and_return(dummie_uuid)
-          end
-
-          let(:dummie_uuid) { "6ac44c8a-4221-4814-bb32-fa16f965b7fd" }
-
-          context "when the leads provides additional notes" do
-            let(:payload) do
-              {
-                type: Events::CalWebhookReceived::KnownBookingTypes::CAREER_CONSULTATION,
-                attendees: [
-                  {
-                    email: "john@skillarc.com",
-                    firstName: "John",
-                    lastName: "Chabot"
-                  }
-                ],
-                location: "+17403573931",
-                organizer: {
-                  email: "katina@skillarc.com"
+          let(:payload) do
+            {
+              type: Events::CalWebhookReceived::KnownBookingTypes::CAREER_CONSULTATION,
+              attendees: [
+                {
+                  email: "john@skillarc.com",
+                  firstName: "John",
+                  lastName: "Chabot"
                 }
+              ],
+              location: "+17403573931",
+              organizer: {
+                email: "katina@skillarc.com"
               }
-            end
-
-            it "emits a ADD_LEAD command" do
-              expect(message_service)
-                .to receive(:create!)
-                .with(
-                  trace_id:,
-                  context_id: dummie_uuid,
-                  schema: Commands::AddLead::V1,
-                  data: {
-                    email: "john@skillarc.com",
-                    lead_id: dummie_uuid,
-                    phone_number: "+17403573931",
-                    first_name: "John",
-                    last_name: "Chabot",
-                    lead_captured_by: "cal.com"
-                  }
-                )
-              subject
-            end
+            }
           end
 
-          context "when the leads doesn't provides additional notes" do
-            let(:payload) do
-              {
-                type: Events::CalWebhookReceived::KnownBookingTypes::CAREER_CONSULTATION,
-                attendees: [
-                  {
-                    email: "john@skillarc.com",
-                    firstName: "John",
-                    lastName: "Chabot"
-                  }
-                ],
-                location: "+17403573931",
-                organizer: {
-                  email: "katina@skillarc.com"
-                },
-                additionalNotes: "This seems like a cool place!"
-              }
-            end
-
-            it "emits ADD_LEAD and ADD_NOTE commands" do
-              expect(message_service)
-                .to receive(:create!)
-                .with(
-                  trace_id:,
-                  context_id: dummie_uuid,
-                  schema: Commands::AddLead::V1,
-                  data: {
-                    email: "john@skillarc.com",
-                    lead_id: dummie_uuid,
-                    phone_number: "+17403573931",
-                    first_name: "John",
-                    last_name: "Chabot",
-                    lead_captured_by: "cal.com"
-                  }
-                )
-
-              expect(message_service)
-                .to receive(:create!)
-                .with(
-                  trace_id:,
-                  context_id: dummie_uuid,
-                  schema: Commands::AddNote::V1,
-                  data: {
-                    originator: "cal.com",
-                    note: "From John Chabot on the meeting invite: This seems like a cool place!",
-                    note_id: dummie_uuid
-                  }
-                )
-
-              subject
-            end
+          it "emits a add person command" do
+            expect(message_service)
+              .to receive(:create!)
+              .with(
+                trace_id:,
+                person_id: be_a(String),
+                schema: Commands::AddPerson::V2,
+                data: {
+                  user_id: nil,
+                  date_of_birth: nil,
+                  email: "john@skillarc.com",
+                  phone_number: "+17403573931",
+                  first_name: "John",
+                  last_name: "Chabot",
+                  source_kind: People::SourceKind::THIRD_PARTY_INTEGRATION,
+                  source_identifier: "cal.com"
+                }
+              )
+            subject
           end
         end
 

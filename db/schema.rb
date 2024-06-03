a7f4eab3bdbe6ea0aefb359467108606e8909013
John Chabot <johnchabot2013@gmail.com>
2024-06-03 11:32:59 -0400

    Drop user contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_31_191212) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_03_153202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -298,17 +298,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_31_191212) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_contact_notifications_on_user_id"
-  end
-
-  create_table "contact_user_contacts", force: :cascade do |t|
-    t.text "user_id", null: false
-    t.string "preferred_contact", null: false
-    t.string "email"
-    t.string "phone_number"
-    t.string "slack_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_contact_user_contacts_on_user_id"
   end
 
   create_table "credentials", id: :text, force: :cascade do |t|

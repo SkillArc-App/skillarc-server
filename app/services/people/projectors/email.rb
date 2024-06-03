a5311ebe2869d277c911b50ea7aedfeabc24231f
John Chabot <johnchabot2013@gmail.com>
2024-06-03 13:21:07 -0400

    Add projector for people email address

---
+++ app/services/people/projectors/email.rb
@@ -0,0 +1,35 @@
+module People
+  module Projectors
+    class Email < Projector
+      projection_aggregator Aggregates::Person
+
+      class Projection
+        extend Record
+
+        schema do
+          initial_email Either(String, nil)
+          current_email Either(String, nil)
+        end
+      end
+
+      def init
+        Projection.new(
+          initial_email: nil,
+          current_email: nil
+        )
+      end
+
+      on_message Events::PersonAdded::V1 do |message, accumulator|
+        accumulator.with(
+          initial_email: message.data.email,
+          current_email: message.data.email
+        )
+      end
+
+      on_message Events::BasicInfoAdded::V1 do |message, accumulator|
+        accumulator = accumulator.with(initial_email: message.data.email) if accumulator.initial_email.blank?
+        accumulator.with(current_email: message.data.email)
+      end
+    end
+  end
+end

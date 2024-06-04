b05b43b6930d107c7c6bb3a4467be6d4b5b89ebb
John Chabot <johnchabot2013@gmail.com>
2024-06-04 15:42:13 -0400

    Update events for people

--- app/models/events/job_order_candidate_added.rb
+++ app/models/events/job_order_candidate_added.rb
@@ -8,15 +8,31 @@ module Events
           seeker_id Uuid
         end
       end
+
+      class V2
+        extend Messages::Payload
+
+        schema do
+          person_id Uuid
+        end
+      end
     end
 
-    V1 = Messages::Schema.active(
+    V1 = Messages::Schema.inactive(
       type: Messages::EVENT,
       data: Data::V1,
       metadata: Messages::Nothing,
       aggregate: Aggregates::JobOrder,
       message_type: Messages::Types::JobOrders::JOB_ORDER_CANDIDATE_ADDED,
       version: 1
+    )
+    V2 = Messages::Schema.active(
+      type: Messages::EVENT,
+      data: Data::V2,
+      metadata: Messages::Nothing,
+      aggregate: Aggregates::JobOrder,
+      message_type: Messages::Types::JobOrders::JOB_ORDER_CANDIDATE_ADDED,
+      version: 2
     )
   end
 end
--- app/models/events/job_order_candidate_applied.rb
+++ app/models/events/job_order_candidate_applied.rb
@@ -9,15 +9,32 @@ module Events
           applied_at ActiveSupport::TimeWithZone, coerce: Messages::TimeZoneCoercer
         end
       end
+
+      class V2
+        extend Messages::Payload
+
+        schema do
+          person_id Uuid
+          applied_at ActiveSupport::TimeWithZone, coerce: Messages::TimeZoneCoercer
+        end
+      end
     end
 
-    V1 = Messages::Schema.active(
+    V1 = Messages::Schema.inactive(
       type: Messages::EVENT,
       data: Data::V1,
       metadata: Messages::Nothing,
       aggregate: Aggregates::JobOrder,
       message_type: Messages::Types::JobOrders::JOB_ORDER_CANDIDATE_APPLIED,
       version: 1
+    )
+    V2 = Messages::Schema.active(
+      type: Messages::EVENT,
+      data: Data::V2,
+      metadata: Messages::Nothing,
+      aggregate: Aggregates::JobOrder,
+      message_type: Messages::Types::JobOrders::JOB_ORDER_CANDIDATE_APPLIED,
+      version: 2
     )
   end
 end

43da0d6372248f316c020c921c23f6c32c3d9846
John Chabot <johnchabot2013@gmail.com>
2024-06-06 12:26:18 -0400

    Create and update events related to invites

--- app/models/events/employer_invite_accepted.rb
+++ app/models/events/employer_invite_accepted.rb
@@ -11,15 +11,34 @@ module Events
           employer_name String
         end
       end
+
+      class V2
+        extend Messages::Payload
+
+        schema do
+          user_id String
+          invite_email String
+          employer_id Uuid
+          employer_name String
+        end
+      end
     end
 
-    V1 = Messages::Schema.active(
+    V1 = Messages::Schema.inactive(
       type: Messages::EVENT,
       data: Data::V1,
       metadata: Messages::Nothing,
       aggregate: Aggregates::Employer,
-      message_type: Messages::Types::Employers::EMPLOYER_INVITE_ACCEPTED,
+      message_type: Messages::Types::Invite::EMPLOYER_INVITE_ACCEPTED,
       version: 1
+    )
+    V2 = Messages::Schema.active(
+      type: Messages::EVENT,
+      data: Data::V2,
+      metadata: Messages::Nothing,
+      aggregate: Aggregates::Invite,
+      message_type: Messages::Types::Invite::EMPLOYER_INVITE_ACCEPTED,
+      version: 2
     )
   end
 end
---
+++ app/models/events/employer_invite_used_by_wrong_user.rb
@@ -0,0 +1,22 @@
+module Events
+  module EmployerInviteUsedByWrongUser
+    module Data
+      class V1
+        extend Messages::Payload
+
+        schema do
+          user_id String
+        end
+      end
+    end
+
+    V1 = Messages::Schema.active(
+      type: Messages::EVENT,
+      data: Data::V1,
+      metadata: Messages::Nothing,
+      aggregate: Aggregates::Invite,
+      message_type: Messages::Types::Invite::EMPLOYER_INVITE_USED_BY_WRONG_USER,
+      version: 1
+    )
+  end
+end

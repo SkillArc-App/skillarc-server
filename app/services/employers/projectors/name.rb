513a4d3435e7aa938d3a179037e9e8562ec450a8
John Chabot <johnchabot2013@gmail.com>
2024-06-07 11:20:08 -0400

    Add name projector for employer

---
+++ app/services/employers/projectors/name.rb
@@ -0,0 +1,29 @@
+module Employers
+  module Projectors
+    class Name < Projector
+      projection_aggregator Aggregates::Employer
+
+      class Projection
+        extend Record
+
+        schema do
+          name Either(String, nil)
+        end
+      end
+
+      def init
+        Projection.new(
+          name: nil
+        )
+      end
+
+      on_message Events::EmployerCreated::V1 do |message, accumulator|
+        accumulator.with(name: message.data.name)
+      end
+
+      on_message Events::EmployerUpdated::V1 do |message, accumulator|
+        accumulator.with(name: message.data.name)
+      end
+    end
+  end
+end

c67bdc1831f63c63bb9d0f82e2ee10ae436ce6d4
John Chabot <johnchabot2013@gmail.com>
2024-06-10 15:56:20 -0400

    Aggregate invite

--- app/services/invites/invites_aggregator.rb
+++ app/services/invites/invites_aggregator.rb
@@ -2,6 +2,7 @@ module Invites
   class InvitesAggregator < MessageConsumer
     def reset_for_replay
       EmployerInvite.delete_all
+      TrainingProviderInvite.delete_all
     end
 
     on_message Events::EmployerInviteCreated::V1, :sync do |message|
@@ -17,6 +18,25 @@ module Invites
 
     on_message Events::EmployerInviteAccepted::V2, :sync do |message|
       EmployerInvite.update!(
+        message.aggregate.id,
+        used_at: message.occurred_at
+      )
+    end
+
+    on_message Events::TrainingProviderInviteCreated::V1, :sync do |message|
+      TrainingProviderInvite.create!(
+        id: message.aggregate.id,
+        email: message.data.invite_email,
+        first_name: message.data.first_name,
+        last_name: message.data.last_name,
+        training_provider_id: message.data.training_provider_id,
+        training_provider_name: message.data.training_provider_name,
+        role_description: message.data.role_description
+      )
+    end
+
+    on_message Events::TrainingProviderInviteAccepted::V2, :sync do |message|
+      TrainingProviderInvite.update!(
         message.aggregate.id,
         used_at: message.occurred_at
       )

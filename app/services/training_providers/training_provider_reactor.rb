6e109cdea05bd23665f19810a471c5fc11c24475
John Chabot <johnchabot2013@gmail.com>
2024-06-11 11:26:34 -0400

    Remove pseudo commands from TP reactor

--- app/services/training_providers/training_provider_reactor.rb
+++ app/services/training_providers/training_provider_reactor.rb
@@ -3,29 +3,5 @@ module TrainingProviders
     def can_replay?
       true
     end
-
-    def create_reference(reference_text:, seeker_id:, author_training_provider_profile_id:, trace_id:)
-      message_service.create!(
-        schema: Events::ReferenceCreated::V1,
-        reference_id: SecureRandom.uuid,
-        data: {
-          reference_text:,
-          author_training_provider_profile_id:,
-          seeker_id:
-        },
-        trace_id:
-      )
-    end
-
-    def update_reference(reference_id:, reference_text:, trace_id:)
-      message_service.create!(
-        schema: Events::ReferenceUpdated::V1,
-        reference_id:,
-        trace_id:,
-        data: {
-          reference_text:
-        }
-      )
-    end
   end
 end

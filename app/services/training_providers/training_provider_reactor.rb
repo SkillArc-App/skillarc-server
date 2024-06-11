93907fd6f24106919739bfbf1bddda8b90c15ce1
John Chabot <johnchabot2013@gmail.com>
2024-06-11 11:54:58 -0400

    Update TP reactor for new commands

--- app/services/training_providers/training_provider_reactor.rb
+++ app/services/training_providers/training_provider_reactor.rb
@@ -3,5 +3,51 @@ module TrainingProviders
     def can_replay?
       true
     end
+
+    on_message Commands::CreateTrainingProvider::V1 do |message|
+      message_service.create_once_for_aggregate!(
+        trace_id: message.trace_id,
+        aggregate: message.aggregate,
+        schema: Events::TrainingProviderCreated::V1,
+        data: {
+          name: message.data.name,
+          description: message.data.description
+        }
+      )
+    end
+
+    on_message Commands::CreateTrainingProviderProgram::V1 do |message|
+      messages = MessageService.aggregate_events(message.aggregate).select { |m| m.occurred_at <= message.occurred_at }
+      return unless Projectors::Aggregates::HasOccurred.new(schema: Events::TrainingProviderCreated::V1).project(messages)
+
+      message_service.create_once_for_trace!(
+        trace_id: message.trace_id,
+        aggregate: message.aggregate,
+        schema: Events::TrainingProviderProgramCreated::V1,
+        data: {
+          program_id: message.data.program_id,
+          name: message.data.name,
+          description: message.data.description
+        }
+      )
+    end
+
+    on_message Commands::UpdateTrainingProviderProgram::V1 do |message|
+      return unless MessageService.aggregate_events(message.aggregate)
+                                  .select { |m| m.occurred_at <= message.occurred_at }
+                                  .select { |m| m.schema == Events::TrainingProviderProgramCreated::V1 }
+                                  .detect { |m| m.data.program_id == message.data.program_id }
+
+      message_service.create_once_for_trace!(
+        trace_id: message.trace_id,
+        aggregate: message.aggregate,
+        schema: Events::TrainingProviderProgramUpdated::V1,
+        data: {
+          program_id: message.data.program_id,
+          name: message.data.name,
+          description: message.data.description
+        }
+      )
+    end
   end
 end
--- spec/services/training_providers/training_provider_reactor_spec.rb
+++ spec/services/training_providers/training_provider_reactor_spec.rb
@@ -5,4 +5,173 @@ RSpec.describe TrainingProviders::TrainingProviderReactor do
 
   let(:instance) { described_class.new(message_service:) }
   let(:message_service) { MessageService.new }
+  let(:messages) { [] }
+
+  describe "#handle_message" do
+    subject do
+      instance.handle_message(message)
+      instance.handle_message(message)
+    end
+
+    before do
+      messages.each do |message|
+        Event.from_message!(message)
+      end
+    end
+
+    context "when the message is create training provider" do
+      let(:message) do
+        build(
+          :message,
+          schema: Commands::CreateTrainingProvider::V1,
+          data: {
+            name: "A",
+            description: "D"
+          }
+        )
+      end
+
+      it "emits a TrainingProviderCreated event" do
+        expect(message_service)
+          .to receive(:create_once_for_aggregate!)
+          .with(
+            schema: Events::TrainingProviderCreated::V1,
+            aggregate: message.aggregate,
+            trace_id: message.trace_id,
+            data: {
+              name: message.data.name,
+              description: message.data.description
+            }
+          )
+          .twice
+          .and_call_original
+
+        subject
+      end
+    end
+
+    context "when the message is create training provider program" do
+      let(:message) do
+        build(
+          :message,
+          schema: Commands::CreateTrainingProviderProgram::V1,
+          aggregate:,
+          data: {
+            program_id: SecureRandom.uuid,
+            name: "N",
+            description: "D"
+          }
+        )
+      end
+
+      let(:aggregate) { Aggregates::TrainingProvider.new(training_provider_id: SecureRandom.uuid) }
+
+      context "when there has been a traing provider created" do
+        let(:messages) do
+          [
+            build(
+              :message,
+              schema: Events::TrainingProviderCreated::V1,
+              aggregate:,
+              data: {
+                name: "A",
+                description: "D"
+              },
+              occurred_at: message.occurred_at - 1.day
+            )
+          ]
+        end
+
+        it "emits a training provider program created event" do
+          expect(message_service)
+            .to receive(:create_once_for_trace!)
+            .with(
+              trace_id: message.trace_id,
+              aggregate: message.aggregate,
+              schema: Events::TrainingProviderProgramCreated::V1,
+              data: {
+                program_id: message.data.program_id,
+                name: message.data.name,
+                description: message.data.description
+              }
+            )
+            .twice
+            .and_call_original
+
+          subject
+        end
+      end
+
+      context "when there has not been a traing provider created" do
+        let(:message_service) { double }
+
+        it "does nothing" do
+          subject
+        end
+      end
+    end
+
+    context "when the message is update training provider program" do
+      let(:message) do
+        build(
+          :message,
+          schema: Commands::UpdateTrainingProviderProgram::V1,
+          aggregate:,
+          data: {
+            program_id: program_id,
+            name: "N",
+            description: "D"
+          }
+        )
+      end
+
+      let(:program_id) { SecureRandom.uuid }
+      let(:aggregate) { Aggregates::TrainingProvider.new(training_provider_id: SecureRandom.uuid) }
+
+      context "when there has been a traing provider program created" do
+        let(:messages) do
+          [
+            build(
+              :message,
+              schema: Events::TrainingProviderProgramCreated::V1,
+              aggregate:,
+              data: {
+                program_id:,
+                name: "A",
+                description: "D"
+              },
+              occurred_at: message.occurred_at - 1.day
+            )
+          ]
+        end
+
+        it "emits a training provider program created event" do
+          expect(message_service)
+            .to receive(:create_once_for_trace!)
+            .with(
+              trace_id: message.trace_id,
+              aggregate: message.aggregate,
+              schema: Events::TrainingProviderProgramUpdated::V1,
+              data: {
+                program_id: message.data.program_id,
+                name: message.data.name,
+                description: message.data.description
+              }
+            )
+            .twice
+            .and_call_original
+
+          subject
+        end
+      end
+
+      context "when there has not been a traing provider program created" do
+        let(:message_service) { double }
+
+        it "does nothing" do
+          subject
+        end
+      end
+    end
+  end
 end

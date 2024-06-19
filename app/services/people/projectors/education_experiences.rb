e28069a1cc697716b9248e6e2c29280bbf661bc8
John Chabot <johnchabot2013@gmail.com>
2024-06-19 12:41:21 -0400

    Add resume related projectors

---
+++ app/services/people/projectors/education_experiences.rb
@@ -0,0 +1,45 @@
+module People
+  module Projectors
+    class EducationExperiences < Projector
+      projection_aggregator Aggregates::Person
+
+      class EducationExperience
+        extend Record
+
+        schema do
+          organization_name Either(String, nil)
+          title Either(String, nil)
+          activities Either(String, nil)
+          graduation_date Either(String, nil)
+          gpa Either(String, nil)
+        end
+      end
+
+      class Projection
+        extend Record
+
+        schema do
+          education_experiences HashOf(String => EducationExperience)
+        end
+      end
+
+      def init
+        Projection.new(
+          education_experiences: {}
+        )
+      end
+
+      on_message Events::EducationExperienceAdded::V2 do |message, accumulator|
+        new_education_experience = EducationExperience.new(
+          organization_name: message.data.organization_name,
+          title: message.data.title,
+          activities: message.data.activities,
+          graduation_date: message.data.graduation_date,
+          gpa: message.data.gpa
+        )
+
+        accumulator.with(education_experiences: accumulator.education_experiences.merge({ message.data.id => new_education_experience }))
+      end
+    end
+  end
+end
---
+++ spec/services/people/projectors/work_experiences_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+
+RSpec.describe People::Projectors::WorkExperiences do
+  describe ".project" do
+    subject { described_class.new.project(messages) }
+
+    let(:aggregate) { Aggregates::Person.new(person_id:) }
+    let(:person_id) { SecureRandom.uuid }
+    let(:work_experience_id1) { SecureRandom.uuid }
+    let(:work_experience_id2) { SecureRandom.uuid }
+
+    let(:messages) do
+      [
+        experience_added1,
+        experience_added2,
+        experience_added3
+      ]
+    end
+
+    let(:experience_added1) do
+      build(
+        :message,
+        aggregate:,
+        schema: Events::ExperienceAdded::V2,
+        data: {
+          id: work_experience_id1,
+          organization_name: "1",
+          position: "1",
+          start_date: "1",
+          end_date: "1",
+          description: "1",
+          is_current: true
+        }
+      )
+    end
+    let(:experience_added2) do
+      build(
+        :message,
+        aggregate:,
+        schema: Events::ExperienceAdded::V2,
+        data: {
+          id: work_experience_id1,
+          organization_name: "2",
+          position: "2",
+          start_date: "2",
+          end_date: "2",
+          description: "2",
+          is_current: true
+        }
+      )
+    end
+    let(:experience_added3) do
+      build(
+        :message,
+        aggregate:,
+        schema: Events::ExperienceAdded::V2,
+        data: {
+          id: work_experience_id2,
+          organization_name: "3",
+          position: "3",
+          start_date: "3",
+          end_date: "3",
+          description: "3",
+          is_current: true
+        }
+      )
+    end
+
+    it "projects the work experience" do
+      expect(subject.work_experiences.length).to eq(2)
+      expect(subject.work_experiences[work_experience_id1]).to eq(
+        described_class::WorkExperience.new(
+          organization_name: "2",
+          position: "2",
+          start_date: "2",
+          end_date: "2",
+          description: "2",
+          is_current: true
+        )
+      )
+      expect(subject.work_experiences[work_experience_id2]).to eq(
+        described_class::WorkExperience.new(
+          organization_name: "3",
+          position: "3",
+          start_date: "3",
+          end_date: "3",
+          description: "3",
+          is_current: true
+        )
+      )
+    end
+  end
+end

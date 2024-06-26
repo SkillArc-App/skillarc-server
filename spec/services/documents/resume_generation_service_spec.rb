86301f25f235e6bfa6f46b71e772706326b515e5
John Chabot <johnchabot2013@gmail.com>
2024-06-26 13:42:02 -0400

    Update resume generation service to make a real resume

--- spec/services/documents/resume_generation_service_spec.rb
+++ spec/services/documents/resume_generation_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Documents::ResumeGenerationService do
   describe ".generate_from_command" do
-    subject { described_class.generate_from_command(message:) }
+    subject { described_class.generate_from_command(message:, gateway:) }
 
     let(:message) do
       build(
@@ -28,7 +28,18 @@ RSpec.describe Documents::ResumeGenerationService do
       )
     end
 
+    let(:gateway) { Documents::Generation::FakeCommunicator.new }
+
     it "creates a pdf document" do
+      expect(gateway)
+        .to receive(:generate_pdf_from_html)
+        .with(
+          document: be_a(String),
+          header: be_a(String),
+          footer: be_a(String)
+        )
+        .and_call_original
+
       expect(subject).to be_a(String)
     end
   end

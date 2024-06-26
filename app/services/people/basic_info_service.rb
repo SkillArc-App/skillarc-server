9e8607b32154a2ffa1334a43416fd3115e17430d
John Chabot <johnchabot2013@gmail.com>
2024-06-26 10:21:47 -0400

    Fix basic info service

--- app/services/people/basic_info_service.rb
+++ app/services/people/basic_info_service.rb
@@ -5,12 +5,11 @@ module People
       @message_service = message_service
     end
 
-    def update(about:, first_name:, last_name:, phone_number:, zip_code:)
+    def update(about:, first_name:, last_name:, phone_number:, email:, zip_code:) # rubocop:disable Metrics/ParameterLists
       seeker = user.seeker
       return unless seeker
 
       aggregate = Aggregates::Person.new(person_id: seeker.id)
-      email = ::Projectors::Aggregates::GetLast.project(schema: Events::BasicInfoAdded::V1, aggregate:)&.data&.email
 
       message_service.create!(
         schema: Events::BasicInfoAdded::V1,
--- spec/services/people/basic_info_service_spec.rb
+++ spec/services/people/basic_info_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe People::BasicInfoService do
   describe ".update" do
-    subject { described_class.new(user_id, message_service).update(about:, first_name:, last_name:, phone_number:, zip_code:) }
+    subject { described_class.new(user_id, message_service).update(about:, first_name:, last_name:, phone_number:, zip_code:, email:) }
 
     let(:message_service) { MessageService.new }
     let(:user_id) { user.id }
@@ -12,29 +12,9 @@ RSpec.describe People::BasicInfoService do
     let(:first_name) { "New First Name" }
     let(:last_name) { "New Last Name" }
     let(:phone_number) { "1234567890" }
+    let(:email) { "A@B.C" }
     let(:zip_code) { "12345" }
     let(:aggregate) { Aggregates::Person.new(person_id: seeker.id) }
-
-    before do
-      allow(MessageService)
-        .to receive(:aggregate_events)
-        .with(aggregate)
-        .and_return(
-          [
-            build(
-              :message,
-              schema: Events::BasicInfoAdded::V1,
-              aggregate_id: seeker.id,
-              data: {
-                first_name: "A name",
-                last_name: "A name",
-                phone_number: "333-333-3333",
-                email: "cool@beans.com"
-              }
-            )
-          ]
-        )
-    end
 
     it "publishes a basic info added event" do
       allow(message_service).to receive(:create!)
@@ -45,7 +25,7 @@ RSpec.describe People::BasicInfoService do
           first_name:,
           last_name:,
           phone_number:,
-          email: "cool@beans.com"
+          email:
         }
       ).and_call_original
 

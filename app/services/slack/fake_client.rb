dc87e63c0635d66693aadb56d21c08af41864958
John Chabot <johnchabot2013@gmail.com>
2024-07-08 12:15:17 -0400

    Dump blocks in fake client

--- app/services/slack/fake_client.rb
+++ app/services/slack/fake_client.rb
@@ -1,7 +1,7 @@
 module Slack
   class FakeClient
     def chat_postMessage(**kwargs) # rubocop:disable Naming/MethodName
-      Rails.logger("[Slack Message to #{kwargs[:channel]}]: #{kwargs[:text]}")
+      Rails.logger("[Slack Message to #{kwargs[:channel]}]: #{kwargs[:text]} - (blocks #{kwargs[:block].to_json})")
     end
   end
 end

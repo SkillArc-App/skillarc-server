d4f5b9cf69af6746dc3403673075fe0775a748f4
John Chabot <johnchabot2013@gmail.com>
2024-07-08 12:13:48 -0400

    Update slack messages to allow block api

--- app/models/teams/commands/send_slack_message.rb
+++ app/models/teams/commands/send_slack_message.rb
@@ -9,15 +9,37 @@ module Teams
             message String
           end
         end
+
+        class V2
+          extend Core::Payload
+
+          schema do
+            text Either(String, nil), default: nil
+            blocks Either(ArrayOf(Hash), nil), default: nil
+          end
+
+          def initialize(**kwarg)
+            super
+            raise ArgumentError unless text.present? || blocks.present?
+          end
+        end
       end
 
-      V1 = Core::Schema.active(
+      V1 = Core::Schema.inactive(
         type: Core::COMMAND,
         data: Data::V1,
         metadata: Core::Nothing,
         aggregate: Aggregates::Team,
         message_type: MessageTypes::SEND_SLACK_MESSAGE,
         version: 1
+      )
+      V2 = Core::Schema.active(
+        type: Core::COMMAND,
+        data: Data::V2,
+        metadata: Core::Nothing,
+        aggregate: Aggregates::Team,
+        message_type: MessageTypes::SEND_SLACK_MESSAGE,
+        version: 2
       )
     end
   end

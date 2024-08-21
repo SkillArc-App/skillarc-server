03cc455eff565ae3240a3da428f56092e5b61220
John Chabot <johnchabot2013@gmail.com>
2024-08-21 08:52:22 -0400

    Handle empty webhook

--- app/services/contact/cal_dot_com/webhook_service.rb
+++ app/services/contact/cal_dot_com/webhook_service.rb
@@ -9,7 +9,7 @@ module Contact
           integration: "cal.com",
           data: {
             cal_trigger_event_type: webhook["triggerEvent"],
-            payload: webhook["payload"].deep_symbolize_keys
+            payload: (webhook["payload"] || {}).deep_symbolize_keys
           },
           occurred_at: webhook["createdAt"]
         )
